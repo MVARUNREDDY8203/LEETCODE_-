5ceadeb1c9443eed9f57d103191adb77052bc4df
M VARUN  REDDY <94187286+MVARUNREDDY8203@users.noreply.github.com>
2023-08-10 21:32:10 +0530

    Time: 3 ms (91.34%), Space: 14 MB (18.44%) - LeetHub

--- 0081-search-in-rotated-sorted-array-ii/0081-search-in-rotated-sorted-array-ii.cpp
+++ 0081-search-in-rotated-sorted-array-ii/0081-search-in-rotated-sorted-array-ii.cpp
@@ -1,9 +1,25 @@
 class Solution {
 public:
     bool search(vector<int>& nums, int target) {
-        for(int i=0; i<nums.size(); i++){
-            if(nums[i] == target) return true;
+        if(nums[0] == target) return true;
+        if(nums[0] < target){
+            for(int i=0; i<nums.size(); i++){
+                if(nums[i] == target) return true;
+            }
+        }else{
+            for(int i=nums.size()-1; i>=0; i--){
+                if(nums[i] == target) return true;
+            }
         }
         return false;
     }
 };
+// class Solution {
+// public:
+//     bool search(vector<int>& nums, int target) {
+//         for(int i=0; i<nums.size(); i++){
+//             if(nums[i] == target) return true;
+//         }
+//         return false;
+//     }
+// };
