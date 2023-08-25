c7b926035d6748a8c4cc97810a2c2a682b1e7693
M VARUN  REDDY <94187286+MVARUNREDDY8203@users.noreply.github.com>
2023-08-25 18:00:30 +0530

    Time: 0 ms (100%), Space: 7.1 MB (51.21%) - LeetHub

--- 0048-rotate-image/0048-rotate-image.cpp
+++ 0048-rotate-image/0048-rotate-image.cpp
@@ -2,26 +2,48 @@ class Solution {
 public:
     void rotate(vector<vector<int>>& matrix) {
         int n = matrix.size();
-        vector<stack<int>> dp(n);
         for(int i=0; i<n; i++){
-            for(int j=0; j<n; j++){
-                dp[j].push(matrix[i][j]);
+            for(int j=0; j<i; j++){
+                swap(matrix[i][j], matrix[j][i]);
             }
         }
-        vector<vector<int>> ans(n, vector<int>(n));
+
         for(int i=0; i<n; i++){
-            int j=0; 
-            while(!dp[i].empty()){
-                ans[i][j++] = dp[i].top();
-                dp[i].pop();
-            }
-        }
-        
-        for(int i=0; i<n; i++){
-            for(int j=0; j<n; j++){
-                matrix[i][j] = ans[i][j];
+            int l = 0;
+            int r = n-1;
+            while(l<=r){
+                swap(matrix[i][l++], matrix[i][r--]);
             }
         }
         return;
     }
 };
+
+
+// class Solution {
+// public:
+//     void rotate(vector<vector<int>>& matrix) {
+//         int n = matrix.size();
+//         vector<stack<int>> dp(n);
+//         for(int i=0; i<n; i++){
+//             for(int j=0; j<n; j++){
+//                 dp[j].push(matrix[i][j]);
+//             }
+//         }
+//         vector<vector<int>> ans(n, vector<int>(n));
+//         for(int i=0; i<n; i++){
+//             int j=0; 
+//             while(!dp[i].empty()){
+//                 ans[i][j++] = dp[i].top();
+//                 dp[i].pop();
+//             }
+//         }
+        
+//         for(int i=0; i<n; i++){
+//             for(int j=0; j<n; j++){
+//                 matrix[i][j] = ans[i][j];
+//             }
+//         }
+//         return;
+//     }
+// };
