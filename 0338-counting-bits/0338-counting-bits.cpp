4defa88ed47353708b18169b6af17bbab59b5b5f
M VARUN  REDDY <94187286+MVARUNREDDY8203@users.noreply.github.com>
2023-09-02 15:06:21 +0530

    Time: 0 ms (100%), Space: 7.8 MB (89.85%) - LeetHub

--- 0338-counting-bits/0338-counting-bits.cpp
+++ 0338-counting-bits/0338-counting-bits.cpp
@@ -1,14 +1,18 @@
 class Solution {
 public:
-    int no_of_ones_in_binary(int n){
-        if(n == 0) return 0;
-        if(n == 1) return 1;
-        return n%2 + no_of_ones_in_binary(n/2);
-    }
+    // int no_of_ones_in_binary(int n){
+    //     if(n == 0) return 0;
+    //     if(n == 1) return 1;
+    //     return n%2 + no_of_ones_in_binary(n/2);
+    // }
     vector<int> countBits(int n) {
         vector<int> ans(n+1,0);
-        for(int i=0; i<=n; i++){
-            ans[i] = no_of_ones_in_binary(i);
+        if(n == 0) return ans;
+        ans[1] = 1;
+        if(n == 1) return ans;
+        
+        for(int i=2; i<=n; i++){
+            ans[i] = ans[i/2] + i%2;
         }
         return ans;
     }
