3bae7f4d9951ccb22740204b58774c88610c9e8b
M VARUN  REDDY <94187286+MVARUNREDDY8203@users.noreply.github.com>
2023-09-03 12:47:12 +0530

    Time: 127 ms (16.67%), Space: 63.8 MB (16.67%) - LeetHub

--- 2954-maximum-sum-of-almost-unique-subarray/2954-maximum-sum-of-almost-unique-subarray.cpp
+++ 2954-maximum-sum-of-almost-unique-subarray/2954-maximum-sum-of-almost-unique-subarray.cpp
@@ -1,30 +1,57 @@
 class Solution {
 public:
     long long maxSum(vector<int>& nums, int m, int k) {
-        
-        int n = nums.size();
-        long long int max_sum = 0;
-        long long int loc_sum = 0;
-        queue<int> que;
-        unordered_map<int, int> mp;
-        
-        for(int i = 0; i<n; i++){
+        long long max_sum = 0, loc_sum = 0;
+        int i=0;
+        unordered_map<int, int> ump;
 
+        while(i < nums.size()){
             loc_sum += nums[i];
-            mp[nums[i]]++;
-            que.push(nums[i]);
-
-            if(que.size() > k){
-                mp[que.front()]--;
-                if(mp[que.front()] < 1) mp.erase(que.front());
-                loc_sum -= que.front();
-                que.pop();
+            ump[nums[i]]++;
+            
+            if(i >= k){
+                loc_sum -= nums[i-k];
+                ump[nums[i-k]]--;
+                if(ump[nums[i-k]] < 1) ump.erase(nums[i-k]);
             }
 
-            if(mp.size() >= m){
+            if(ump.size() >= m){
                 max_sum = max(max_sum, loc_sum);
             }
+
+            i++;
         }
         return max_sum;
     }
 };
+
+// class Solution {
+// public:
+//     long long maxSum(vector<int>& nums, int m, int k) {
+     
+//         int n = nums.size();
+//         long long int max_sum = 0;
+//         long long int loc_sum = 0;
+//         queue<int> que;
+//         unordered_map<int, int> mp;
+        
+//         for(int i = 0; i<n; i++){
+
+//             loc_sum += nums[i];
+//             mp[nums[i]]++;
+//             que.push(nums[i]);
+
+//             if(que.size() > k){
+//                 mp[que.front()]--;
+//                 if(mp[que.front()] < 1) mp.erase(que.front());
+//                 loc_sum -= que.front();
+//                 que.pop();
+//             }
+
+//             if(mp.size() >= m){
+//                 max_sum = max(max_sum, loc_sum);
+//             }
+//         }
+//         return max_sum;
+//     }
+// };
