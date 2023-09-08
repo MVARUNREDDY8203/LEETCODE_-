6979ba548307cc14f9d103bf6b72b9f90d33bb4b
M VARUN  REDDY <94187286+MVARUNREDDY8203@users.noreply.github.com>
2023-09-08 15:51:10 +0530

    Time: 9 ms (50.08%), Space: 11.6 MB (69.86%) - LeetHub

--- 0083-remove-duplicates-from-sorted-list/0083-remove-duplicates-from-sorted-list.cpp
+++ 0083-remove-duplicates-from-sorted-list/0083-remove-duplicates-from-sorted-list.cpp
@@ -1,16 +1,40 @@
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode() : val(0), next(nullptr) {}
+ *     ListNode(int x) : val(x), next(nullptr) {}
+ *     ListNode(int x, ListNode *next) : val(x), next(next) {}
+ * };
+ */
 class Solution {
 public:
     ListNode* deleteDuplicates(ListNode* head) {
-        ListNode* temp=head;
-        while (temp&&temp->next)
-        {
-            if (temp->next->val==temp->val)
-            {
-                temp->next=temp->next->next;
-                continue;
-            }
-            temp=temp->next;
+        if(head == NULL || head->next == NULL) return head;
+
+        ListNode* curr = head;
+        while(curr && curr->next){
+            if(curr->val == curr->next->val) curr->next = curr->next->next;
+            else curr = curr->next;
         }
         return head;
-    }
+    }   
 };
+
+// class Solution {
+// public:
+//     ListNode* deleteDuplicates(ListNode* head) {
+//         ListNode* temp=head;
+//         while (temp&&temp->next)
+//         {
+//             if (temp->next->val==temp->val)
+//             {
+//                 temp->next=temp->next->next;
+//                 continue;
+//             }
+//             temp=temp->next;
+//         }
+//         return head;
+//     }
+// };
