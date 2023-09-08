e9db0450cf0c4aa6d29123235a967240839f9bd2
M VARUN  REDDY <94187286+MVARUNREDDY8203@users.noreply.github.com>
2023-09-08 15:11:27 +0530

    Time: 3 ms (74.49%), Space: 10.8 MB (19.21%) - LeetHub

--- 0019-remove-nth-node-from-end-of-list/0019-remove-nth-node-from-end-of-list.cpp
+++ 0019-remove-nth-node-from-end-of-list/0019-remove-nth-node-from-end-of-list.cpp
@@ -11,17 +11,39 @@
 class Solution {
 public:
     ListNode* removeNthFromEnd(ListNode* head, int n) {
-        unordered_map<int, ListNode*> ump;
         ListNode* curr = head;
-        int index = 1;
-        while (curr != NULL) {
-            ump[index++] = curr;
+        int size = 0;
+        while(curr != NULL){
             curr = curr->next;
+            size++;
         }
-        ump[index] == NULL;
-        int del = index-n;
-        if(del == 1) return head->next;
-        ump[del-1]->next = ump[del+1];
+        int prev = size - n;
+        if (prev == 0) return head->next;
+        curr = head;
+        while (prev != 1) {
+            curr = curr->next;
+            prev--;
+        }
+
+        curr->next = curr->next->next;
         return head;
     }
 };
+
+// class Solution {
+// public:
+//     ListNode* removeNthFromEnd(ListNode* head, int n) {
+//         unordered_map<int, ListNode*> ump;
+//         ListNode* curr = head;
+//         int index = 1;
+//         while (curr != NULL) {
+//             ump[index++] = curr;
+//             curr = curr->next;
+//         }
+//         ump[index] == NULL;
+//         int del = index-n;
+//         if(del == 1) return head->next;
+//         ump[del-1]->next = ump[del+1];
+//         return head;
+//     }
+// };
