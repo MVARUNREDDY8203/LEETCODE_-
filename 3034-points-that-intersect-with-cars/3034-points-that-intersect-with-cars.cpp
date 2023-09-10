bf0291f1f4ad864c23dbd8ed8cf2a42605da6174
M VARUN  REDDY <94187286+MVARUNREDDY8203@users.noreply.github.com>
2023-09-10 18:43:55 +0530

    Time: 23 ms (25%), Space: 26 MB (12.5%) - LeetHub

---
+++ 3034-points-that-intersect-with-cars/3034-points-that-intersect-with-cars.cpp
@@ -0,0 +1,26 @@
+class Solution {
+public:
+    int numberOfPoints(vector<vector<int>>& nums) {
+        unordered_set<int> ust;
+        for (int i=0; i<nums.size(); i++) {
+            for (int j=nums[i][0]; j<=nums[i][1]; j++) {
+                ust.insert(j);
+            }
+        }
+        return ust.size();
+    }
+};
+
+// class Solution {
+// public:
+//     int numberOfPoints(vector<vector<int>>& nums) {
+//         unordered_set<int> ust;
+//         for(int i=0; i<nums.size(); i++){
+//             for(int j=nums[i][0]; j<=nums[i][1];){
+//                 ust.insert(j);
+//                 j++;
+//             }
+//         }
+//         return ust.size();
+//     }
+// };
