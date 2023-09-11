706cf93456fed8c32f9a49753d12c67dc4153e3a
M VARUN  REDDY <94187286+MVARUNREDDY8203@users.noreply.github.com>
2023-09-11 13:41:00 +0530

    Time: 4 ms (74.92%), Space: 10.2 MB (63.26%) - LeetHub

--- 0086-partition-list/0086-partition-list.cpp
+++ 0086-partition-list/0086-partition-list.cpp
@@ -11,36 +11,58 @@
 class Solution {
 public:
     ListNode* partition(ListNode* head, int x) {
-        if (!head || !head->next) return head;
+        ListNode* lt = new ListNode(-300);
+        ListNode* dummy_lt = lt;
+        ListNode* gte = new ListNode(-300);
+        ListNode* dummy_gte = gte;
         
-        int index1 = 1;
-        int index2 = 1;
-        unordered_map<int, ListNode*> ump_lt;
-        unordered_map<int, ListNode*> ump_gte;
-        ListNode* curr = head;
-
-        while (curr) {
-            if (curr->val >= x) ump_gte[index2++] = curr;
-            else ump_lt[index1++] = curr;
-            curr = curr->next;
+        while (head) {
+            if (head->val < x) {
+                lt->next = head;
+                lt = lt->next;
+            }else {
+                gte->next = head;
+                gte = gte->next;
+            }
+            head = head->next;
         }
-        ump_lt[index1] = NULL;
-        ump_gte[index2] = NULL;
-
-        curr = new ListNode(-300);
-        head = curr;
-        for (int i=1; i<index1; i++) {
-            curr->next = ump_lt[i];
-            curr = curr->next;
-        }
-
-        for (int i=1; i<index2; i++) {
-            curr->next = ump_gte[i];
-            curr = curr->next;
-        }
-
-        curr->next = NULL;
-        return head->next;
+        lt->next = NULL;
+        gte->next = NULL;
         
+        if (!dummy_lt->next) return dummy_gte->next;
+        lt->next = dummy_gte->next;
+        return dummy_lt->next;
     }
 };
+// ListNode* partition(ListNode* head, int x) {
+//     if (!head || !head->next) return head;
+    
+//     int index1 = 1;
+//     int index2 = 1;
+//     unordered_map<int, ListNode*> ump_lt;
+//     unordered_map<int, ListNode*> ump_gte;
+//     ListNode* curr = head;
+
+//     while (curr) {
+//         if (curr->val >= x) ump_gte[index2++] = curr;
+//         else ump_lt[index1++] = curr;
+//         curr = curr->next;
+//     }
+//     ump_lt[index1] = NULL;
+//     ump_gte[index2] = NULL;
+
+//     curr = new ListNode(-300);
+//     head = curr;
+//     for (int i=1; i<index1; i++) {
+//         curr->next = ump_lt[i];
+//         curr = curr->next;
+//     }
+
+//     for (int i=1; i<index2; i++) {
+//         curr->next = ump_gte[i];
+//         curr = curr->next;
+//     }
+
+//     curr->next = NULL;
+//     return head->next;  
+// }
