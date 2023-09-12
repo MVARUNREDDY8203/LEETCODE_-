78d01c4c98c6aa23aa202a9d3b95be729b608a29
M VARUN  REDDY <94187286+MVARUNREDDY8203@users.noreply.github.com>
2023-09-12 15:36:01 +0530

    Time: 35 ms (5.77%), Space: 13.2 MB (5.77%) - LeetHub

---
+++ 0025-reverse-nodes-in-k-group/0025-reverse-nodes-in-k-group.cpp
@@ -0,0 +1,49 @@
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode() : val(0), next(nullptr) {}
+ *     ListNode(int x) : val(x), next(nullptr) {}
+ *     ListNode(int x, ListNode *next) : val(x), next(next) {}
+ * };
+ */
+class Solution {
+public:
+    ListNode* reverseKGroup(ListNode* head, int k) {
+        if (!head->next || k == 1) return head;
+
+        unordered_map<int, ListNode*> ump;
+        ListNode* curr = head;
+        int index = 1;
+        while (curr) {
+            ump[index++] = curr;
+            curr = curr->next;
+        }
+        // ump[index] = NULL;
+        
+        int i2 = k;
+        while (i2 <= index) {
+            curr = ump[i2];
+            int i = i2-1;
+            while (i > i2-k) {
+                cout<<curr->val<<" ";
+                curr->next = ump[i--];
+                curr = curr->next;
+                cout<<curr->val<<" ";
+            }
+            i2 += k;
+            if (ump.find(i2) != ump.end()) curr->next = ump[i2];
+            else break;
+        }
+        if (--index % k == 0) {
+            curr->next = NULL;
+        }else{
+            i2 -= k;
+            curr->next = ump[++i2];
+        }
+
+
+        return ump[k];
+    }
+};
