c4965acf542a59ee5281d04b43d2907e06007b7a
M VARUN  REDDY <94187286+MVARUNREDDY8203@users.noreply.github.com>
2023-09-13 12:39:19 +0530

    Time: 16 ms (40.61%), Space: 11.5 MB (58.1%) - LeetHub

--- 0025-reverse-nodes-in-k-group/0025-reverse-nodes-in-k-group.cpp
+++ 0025-reverse-nodes-in-k-group/0025-reverse-nodes-in-k-group.cpp
@@ -11,38 +11,56 @@
 class Solution {
 public:
     ListNode* reverseKGroup(ListNode* head, int k) {
-        if (!head->next || k == 1) return head;
-
-        unordered_map<int, ListNode*> ump;
         ListNode* curr = head;
-        int index = 1;
-        while (curr) {
-            ump[index++] = curr;
+        for (int i=0; i<k; i++) {
+            if (!curr) return head;
             curr = curr->next;
         }
-
-        int i2 = k;
-        while (i2 <= index) {
-            curr = ump[i2];
-            int i = i2-1;
-            while (i > i2-k) {
-                cout<<curr->val<<" ";
-                curr->next = ump[i--];
-                curr = curr->next;
-                cout<<curr->val<<" ";
-            }
-            i2 += k;
-            if (ump.find(i2) != ump.end()) curr->next = ump[i2];
-            else break;
+        curr = head;
+        ListNode* prev = NULL;
+        ListNode* next = NULL;
+        for (int i=0; i<k; i++) {
+            next = curr->next;
+            curr->next = prev;
+            prev = curr;
+            curr = next;
         }
-
-        if (--index % k == 0) {
-            curr->next = NULL;
-        }else{
-            i2 -= k;
-            curr->next = ump[++i2];
-        }
-
-        return ump[k];
+        head->next = reverseKGroup(curr, k);
+        return prev;
     }
 };
+// ListNode* reverseKGroup(ListNode* head, int k) {
+//     if (!head->next || k == 1) return head;
+
+//     unordered_map<int, ListNode*> ump;
+//     ListNode* curr = head;
+//     int index = 1;
+//     while (curr) {
+//         ump[index++] = curr;
+//         curr = curr->next;
+//     }
+
+//     int i2 = k;
+//     while (i2 <= index) {
+//         curr = ump[i2];
+//         int i = i2-1;
+//         while (i > i2-k) {
+//             cout<<curr->val<<" ";
+//             curr->next = ump[i--];
+//             curr = curr->next;
+//             cout<<curr->val<<" ";
+//         }
+//         i2 += k;
+//         if (ump.find(i2) != ump.end()) curr->next = ump[i2];
+//         else break;
+//     }
+
+//     if (--index % k == 0) {
+//         curr->next = NULL;
+//     }else{
+//         i2 -= k;
+//         curr->next = ump[++i2];
+//     }
+
+//     return ump[k];
+// }
