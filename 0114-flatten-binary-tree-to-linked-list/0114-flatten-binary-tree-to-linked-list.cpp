81790bc6c0b7dcdf6c16eaae48c90faf9e8fddd6
M VARUN  REDDY <94187286+MVARUNREDDY8203@users.noreply.github.com>
2023-09-18 14:14:36 +0530

    Time: 0 ms (100%), Space: 12.9 MB (12%) - LeetHub

--- 0114-flatten-binary-tree-to-linked-list/0114-flatten-binary-tree-to-linked-list.cpp
+++ 0114-flatten-binary-tree-to-linked-list/0114-flatten-binary-tree-to-linked-list.cpp
@@ -11,20 +11,37 @@
  */
 class Solution {
 public:
-    void preorder(TreeNode* root, vector<TreeNode*> &arr) {
-        if (!root) return;
-        arr.push_back(root);
-        preorder(root->left, arr);
-        preorder(root->right, arr);
+    TreeNode* preorderer(TreeNode* root) {
+        if (!root) return NULL;
+        TreeNode* left_tree = preorderer(root->left);
+        TreeNode* right_tree = preorderer(root->right);
+        root->left = NULL;
+        root->right = left_tree;
+        TreeNode* curr = root;
+        while (curr && curr->right) {
+            curr = curr->right;
+        }
+        curr->right = right_tree;
+        return root;
     }
     void flatten(TreeNode* root) {
         if (!root) return;
-        vector<TreeNode*> arr;
-        preorder(root, arr);
-        arr.push_back(NULL);
-        for (int i=0; i<arr.size()-1; i++) {
-            arr[i]->right = arr[i+1];
-            arr[i]->left = NULL;
-        }
+        root = preorderer(root);
     }
 };
+// void preorder(TreeNode* root, vector<TreeNode*> &arr) {
+//     if (!root) return;
+//     arr.push_back(root);
+//     preorder(root->left, arr);
+//     preorder(root->right, arr);
+// }
+// void flatten(TreeNode* root) {
+//     if (!root) return;
+//     vector<TreeNode*> arr;
+//     preorder(root, arr);
+//     arr.push_back(NULL);
+//     for (int i=0; i<arr.size()-1; i++) {
+//         arr[i]->right = arr[i+1];
+//         arr[i]->left = NULL;
+//     }
+// }
