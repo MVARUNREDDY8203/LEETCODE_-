88b73abcc6c504aea40dd4fca414b87430de82fb
M VARUN  REDDY <94187286+MVARUNREDDY8203@users.noreply.github.com>
2023-09-18 10:26:26 +0530

    Time: 9 ms (66.67%), Space: 22.3 MB (33.33%) - LeetHub

--- 3093-sum-of-values-at-indices-with-k-set-bits/3093-sum-of-values-at-indices-with-k-set-bits.cpp
+++ 3093-sum-of-values-at-indices-with-k-set-bits/3093-sum-of-values-at-indices-with-k-set-bits.cpp
@@ -1,19 +1,17 @@
 class Solution {
 public:
     int sumIndicesWithKSetBits(vector<int>& nums, int k) {
-        vector<int> binary(100001, 0);
+        vector<int> binary(nums.size()+1, 0);
         binary[0] = 0;
         binary[1] = 1;
-        for(int i=2; i<=100000; i++){
+        for(int i=2; i<=nums.size(); i++){
             binary[i] = binary[i/2] + i%2;
         }
         
         int count = 0;
         for(int i=0; i<nums.size(); i++){
-            cout<<nums[i]<<" "<<binary[nums[i]]<<" ";
             if(binary[i] == k) count+=nums[i];
         }
-        cout<<endl;
         return count;
     }
 };
