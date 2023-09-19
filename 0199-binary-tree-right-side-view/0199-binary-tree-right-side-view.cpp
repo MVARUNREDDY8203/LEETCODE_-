3011dc272c3518fc598e7a3c7a66cd93a6256a74
M VARUN  REDDY <94187286+MVARUNREDDY8203@users.noreply.github.com>
2023-09-19 18:27:01 +0530

    Time: 3 ms (61.73%), Space: 12.3 MB (8.65%) - LeetHub

--- 0199-binary-tree-right-side-view/0199-binary-tree-right-side-view.cpp
+++ 0199-binary-tree-right-side-view/0199-binary-tree-right-side-view.cpp
@@ -14,25 +14,42 @@ public:
     vector<int> rightSideView(TreeNode* root) {
         if (!root) return {};
 
-        vector<vector<TreeNode*>> lvls;
-        lvls.push_back({root});
-        int i = 0;
-        while (true) {
-            vector<TreeNode*> temp;
-            for (int j=0; j<lvls[i].size(); j++) {
-                if(lvls[i][j]->left) temp.push_back(lvls[i][j]->left);
-                if(lvls[i][j]->right) temp.push_back(lvls[i][j]->right);
-            }
-            if(temp.size() == 0) break;
-            lvls.push_back(temp);
-            i++;
-        }
-
+        queue<TreeNode*> que;
         vector<int> ans;
-        for (auto i: lvls) {
-            ans.push_back(i.back()->val);
+        que.push(root);
+        while (!que.empty()) {
+            int size = que.size();
+            ans.push_back(que.back()->val);
+            for (int i=0; i<size; i++) {
+                if (que.front()->left) que.push(que.front()->left);
+                if (que.front()->right) que.push(que.front()->right);
+                que.pop();
+            }
         }
-        
         return ans;
     }
 };
+// vector<int> rightSideView(TreeNode* root) {
+//     if (!root) return {};
+
+//     vector<vector<TreeNode*>> lvls;
+//     lvls.push_back({root});
+//     int i = 0;
+//     while (true) {
+//         vector<TreeNode*> temp;
+//         for (int j=0; j<lvls[i].size(); j++) {
+//             if(lvls[i][j]->left) temp.push_back(lvls[i][j]->left);
+//             if(lvls[i][j]->right) temp.push_back(lvls[i][j]->right);
+//         }
+//         if(temp.size() == 0) break;
+//         lvls.push_back(temp);
+//         i++;
+//     }
+
+//     vector<int> ans;
+//     for (auto i: lvls) {
+//         ans.push_back(i.back()->val);
+//     }
+    
+//     return ans;
+// }
