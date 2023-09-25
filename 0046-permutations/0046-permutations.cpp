97f3e132d54b4b79b417b6fb7f068a166ba0b6b6
M VARUN  REDDY <94187286+MVARUNREDDY8203@users.noreply.github.com>
2023-09-25 23:00:50 +0530

    Time: 19 ms (5.01%), Space: 14.5 MB (5.13%) - LeetHub

--- 0046-permutations/0046-permutations.cpp
+++ 0046-permutations/0046-permutations.cpp
@@ -1,42 +1,74 @@
 class Solution {
 public:
-    vector<int> next_permutation_of(vector<int> nums) {
-        int i=nums.size()-1;
-        for (i=i; i>0; i--) {
-            if (nums[i] > nums[i-1]) break;
+    vector<vector<int>> all_permutations;
+    vector<unordered_set<int>> ap_ust;
+    
+    void ap_util(vector<int> &nums, vector<int> curr, unordered_set<int> ust){
+        for (auto i: curr) {
+            cout<<i<<" ";
         }
-        i--;
-        if (i < 0) {
-            reverse(nums.begin(), nums.end());
+        cout<<endl;
+        if (curr.size() == nums.size()) {
+            all_permutations.push_back(curr);
+            return;
         }
-        else {
-            for (int j=nums.size()-1; j>i; j--) {
-                if (nums[j] > nums[i]) {
-                    swap(nums[j], nums[i]);
-                    reverse(nums.begin()+i+1, nums.end());
-                    break;
-                }
-            }
+        for (auto i: nums) {
+            if (ust.find(i) != ust.end()) continue;
+            vector<int> temp = curr;
+            temp.push_back(i);
+            unordered_set<int> temp_ust = ust;
+            temp_ust.insert(i);
+            ap_util(nums, temp, temp_ust);
         }
-        return nums;
-    }
-    int factorial (int n) {
-        if (n <= 0) return 1;
-        return n * factorial(n-1);
     }
     vector<vector<int>> permute(vector<int>& nums) {
-        int total_permutations = factorial(nums.size());
-        vector<vector<int>> ans;
-        vector<int> temp;
-        ans.push_back(nums);
-
-        if(nums.size() == 1) return ans;
-
-        while (total_permutations-1 > 0) {
-            ans.push_back(next_permutation_of(ans.back()));
-
-            total_permutations--;
+        for (auto i: nums) {
+            vector<int> temp;
+            unordered_set<int> ust;
+            temp.push_back(i);
+            ust.insert(i);
+            ap_util(nums, temp, ust);
         }
-        return ans;
+        // ap_util(nums, temp, ust);
+        return all_permutations;
     }
 };
+    // vector<int> next_permutation_of(vector<int> nums) {
+    //     int i=nums.size()-1;
+    //     for (i=i; i>0; i--) {
+    //         if (nums[i] > nums[i-1]) break;
+    //     }
+    //     i--;
+    //     if (i < 0) {
+    //         reverse(nums.begin(), nums.end());
+    //     }
+    //     else {
+    //         for (int j=nums.size()-1; j>i; j--) {
+    //             if (nums[j] > nums[i]) {
+    //                 swap(nums[j], nums[i]);
+    //                 reverse(nums.begin()+i+1, nums.end());
+    //                 break;
+    //             }
+    //         }
+    //     }
+    //     return nums;
+    // }
+    // int factorial (int n) {
+    //     if (n <= 0) return 1;
+    //     return n * factorial(n-1);
+    // }
+    // vector<vector<int>> permute(vector<int>& nums) {
+    //     int total_permutations = factorial(nums.size());
+    //     vector<vector<int>> ans;
+    //     vector<int> temp;
+    //     ans.push_back(nums);
+
+    //     if(nums.size() == 1) return ans;
+
+    //     while (total_permutations-1 > 0) {
+    //         ans.push_back(next_permutation_of(ans.back()));
+
+    //         total_permutations--;
+    //     }
+    //     return ans;
+    // }
