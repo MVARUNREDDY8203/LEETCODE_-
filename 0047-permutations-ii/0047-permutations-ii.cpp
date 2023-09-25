341aaae1b541d0e6a62f76629482ae0d5dc3d926
M VARUN  REDDY <94187286+MVARUNREDDY8203@users.noreply.github.com>
2023-09-26 00:01:24 +0530

    Time: 4 ms (82.78%), Space: 10.1 MB (35.55%) - LeetHub

--- 0047-permutations-ii/0047-permutations-ii.cpp
+++ 0047-permutations-ii/0047-permutations-ii.cpp
@@ -1,51 +1,68 @@
 class Solution {
 public:
-    vector<int> next_permutation_of(vector<int> nums) {
-        int i=nums.size()-1;
-        for (i=i; i>0; i--) {
-            if (nums[i] > nums[i-1]) break;
+    vector<vector<int>> ans;
+    void ap_util(vector<int> nums, int i, int n) {
+        if (i==n) {
+            ans.emplace_back(nums);
+            return;
         }
-        i--;
-        if (i < 0) {
-            reverse(nums.begin(), nums.end());
+        for (int j=i; j<=n; j++) {
+            if (nums[i] == nums[j] && j != i) continue;
+            swap(nums[i], nums[j]);
+            ap_util(nums, i+1, n);
+            // swap(nums[i], nums[j]);
         }
-        else {
-            for (int j=nums.size()-1; j>i; j--) {
-                if (nums[j] > nums[i]) {
-                    swap(nums[j], nums[i]);
-                    reverse(nums.begin()+i+1, nums.end());
-                    break;
-                }
-            }
-        }
-        return nums;
-    }
-    int factorial (int n) {
-        if (n <= 0) return 1;
-        return n * factorial(n-1);
     }
     vector<vector<int>> permuteUnique(vector<int>& nums) {
-        int total_permutations = factorial(nums.size());
-        vector<vector<int>> ans;
-        vector<int> temp;
-        ans.push_back(nums);
-
-        if(nums.size() == 1) return ans;
-
-        while (total_permutations-1 > 0) {
-            // vector<int>
-            ans.push_back(next_permutation_of(ans.back()));
-
-            total_permutations--;
-        }
-        vector<vector<int>> final_ans;
-        set<vector<int>> ust;
-        for (auto i: ans) {
-            if (ust.find(i) == ust.end()) {
-                ust.insert(i);
-                final_ans.push_back(i);
-            }
-        }
-        return final_ans;
+        sort(nums.begin(), nums.end());
+        ap_util(nums, 0, nums.size()-1);
+        return ans;
     }
 };
+    // vector<int> next_permutation_of(vector<int> nums) {
+    //     int i=nums.size()-1;
+    //     for (i=i; i>0; i--) {
+    //         if (nums[i] > nums[i-1]) break;
+    //     }
+    //     i--;
+    //     if (i < 0) {
+    //         reverse(nums.begin(), nums.end());
+    //     }
+    //     else {
+    //         for (int j=nums.size()-1; j>i; j--) {
+    //             if (nums[j] > nums[i]) {
+    //                 swap(nums[j], nums[i]);
+    //                 reverse(nums.begin()+i+1, nums.end());
+    //                 break;
+    //             }
+    //         }
+    //     }
+    //     return nums;
+    // }
+    // int factorial (int n) {
+    //     if (n <= 0) return 1;
+    //     return n * factorial(n-1);
+    // }
+    // vector<vector<int>> permuteUnique(vector<int>& nums) {
+    //     int total_permutations = factorial(nums.size());
+    //     vector<vector<int>> ans;
+    //     vector<int> temp;
+    //     ans.push_back(nums);
+
+    //     if(nums.size() == 1) return ans;
+
+    //     while (total_permutations-1 > 0) {
+    //         ans.push_back(next_permutation_of(ans.back()));
+
+    //         total_permutations--;
+    //     }
+    //     vector<vector<int>> final_ans;
+    //     set<vector<int>> ust;
+    //     for (auto i: ans) {
+    //         if (ust.find(i) == ust.end()) {
+    //             ust.insert(i);
+    //             final_ans.push_back(i);
+    //         }
+    //     }
+    //     return final_ans;
+    // }
