93c9a1695753f5b1105135f70490d4b919acb544
M VARUN  REDDY <94187286+MVARUNREDDY8203@users.noreply.github.com>
2023-09-26 16:04:13 +0530

    Time: 3 ms (65.64%), Space: 7 MB (9.57%) - LeetHub

--- 0389-find-the-difference/0389-find-the-difference.cpp
+++ 0389-find-the-difference/0389-find-the-difference.cpp
@@ -1,13 +1,19 @@
 class Solution {
 public:
     char findTheDifference(string s, string t) {
-        unordered_map<char, int> ump1, ump2;
-        for (auto i: s) ump1[i]++;
-        for (auto i: t) ump2[i]++;
-        for (auto i: t) {
-            if (ump1.find(i) == ump1.end()) return i;
-            if (ump1[i] != ump2[i]) return i;
+        // unordered_map<char, int> ump1, ump2;
+        // for (auto i: s) ump1[i]++;
+        // for (auto i: t) ump2[i]++;
+        // for (auto i: t) {
+        //     if (ump1.find(i) == ump1.end()) return i;
+        //     if (ump1[i] != ump2[i]) return i;
+        // }
+        // return t[0];
+        sort(begin(s), end(s));
+        sort(begin(t), end(t));
+        for(int i=0; i<t.size() && i<s.size(); i++) {
+            if (t[i] != s[i]) return t[i];
         }
-        return t[0];
+        return t[t.size()-1];
     }
 };
