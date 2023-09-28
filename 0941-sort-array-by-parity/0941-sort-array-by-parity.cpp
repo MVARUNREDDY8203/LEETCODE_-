abbb02152c10b5ceacc3ee89aaef4e8e317a80b4
M VARUN  REDDY <94187286+MVARUNREDDY8203@users.noreply.github.com>
2023-09-28 15:02:03 +0530

    Time: 14 ms (13.55%), Space: 16.5 MB (25.63%) - LeetHub

--- 0941-sort-array-by-parity/0941-sort-array-by-parity.cpp
+++ 0941-sort-array-by-parity/0941-sort-array-by-parity.cpp
@@ -1,13 +1,22 @@
 class Solution {
 public:
     vector<int> sortArrayByParity(vector<int>& nums) {
-        vector<int> ans; 
-        for (auto i: nums) {
-            if (i%2 == 0) ans.emplace_back(i);
+        int l = 0;
+        int r = nums.size()-1;
+        while (l <= r) {
+            if (nums[l] % 2 == 1) swap(nums[l], nums[r--]);
+            else l++;
         }
-        for (auto i: nums) {
-            if (i%2 == 1) ans.emplace_back(i);
-        }
-        return ans;
+        return nums;
     }
 };
+// vector<int> sortArrayByParity(vector<int>& nums) {
+//     vector<int> ans; 
+//     for (auto i: nums) {
+//         if (i%2 == 0) ans.emplace_back(i);
+//     }
+//     for (auto i: nums) {
+//         if (i%2 == 1) ans.emplace_back(i);
+//     }
+//     return ans;
+// }
