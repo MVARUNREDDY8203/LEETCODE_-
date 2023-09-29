20aa3d22e0da522cdd178ddbc4d8e462c29f24d2
M VARUN  REDDY <94187286+MVARUNREDDY8203@users.noreply.github.com>
2023-09-29 14:24:14 +0530

    Time: 123 ms (56.83%), Space: 96.9 MB (9.17%) - LeetHub

--- 0932-monotonic-array/0932-monotonic-array.cpp
+++ 0932-monotonic-array/0932-monotonic-array.cpp
@@ -1,19 +1,32 @@
 class Solution {
 public:
     bool isMonotonic(vector<int>& nums) {
-        vector<int> nums2 = nums;
-        sort(nums.begin(), nums.end());
-        bool asc = true;
-        bool desc = true;
-        for (int i=0; i<nums.size(); i++) {
-            if (nums2[i] != nums[i]) {
-                asc = false;
+        if (nums.size() == 1) return true;
+        bool asc = true, desc = true;
+        for (int i=1; i<nums.size(); i++) {
+            if (nums[i] < nums[i-1]) {
+                asc = false; 
             }
-            if (nums2[i] != nums[nums.size()-1-i]) {
-                desc = false;
+            if (nums[i] > nums[i-1]) {
+                desc = false; 
             }
         }
-        
-        return asc || desc;
+        return asc || desc;        
     }
 };
+    // bool isMonotonic(vector<int>& nums) {
+    //     // vector<int> nums2 = nums;
+    //     // sort(nums.begin(), nums.end());
+    //     // bool asc = true;
+    //     // bool desc = true;
+    //     // for (int i=0; i<nums.size(); i++) {
+    //     //     if (nums2[i] != nums[i]) {
+    //     //         asc = false;
+    //     //     }
+    //     //     if (nums2[i] != nums[nums.size()-1-i]) {
+    //     //         desc = false;
+    //     //     }
+    //     // }
+        
+    //     // return asc || desc;
+    // }
