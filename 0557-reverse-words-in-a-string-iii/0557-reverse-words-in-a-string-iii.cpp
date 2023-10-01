796e4118dbc9859eee0e777fe3cf3acc5a4becdf
M VARUN  REDDY <94187286+MVARUNREDDY8203@users.noreply.github.com>
2023-10-01 18:32:17 +0530

    Time: 22 ms (20.61%), Space: 14 MB (20.03%) - LeetHub

---
+++ 0557-reverse-words-in-a-string-iii/0557-reverse-words-in-a-string-iii.cpp
@@ -0,0 +1,21 @@
+class Solution {
+public:
+    string reverseWords(string s) {
+        string temp = "";
+        string ans = "";
+        int i=0;
+        for (i=0; i<s.size(); i++) {
+            if (s[i] == ' ' && temp != "") {
+                reverse(temp.begin(), temp.end());
+                ans += temp + " ";
+                temp = "";
+            }  
+            else temp += s[i];
+        }
+        if (temp != "") {
+            reverse(temp.begin(), temp.end());
+            ans += temp;
+        } 
+        return ans;
+    }
+};
