11671f0c8efa2e9f7542ac0b98b607969605f961
M VARUN  REDDY <94187286+MVARUNREDDY8203@users.noreply.github.com>
2023-10-02 12:58:11 +0530

    Time: 27 ms (93.3%), Space: 13.7 MB (11.45%) - LeetHub

--- 2149-remove-colored-pieces-if-both-neighbors-are-the-same-color/2149-remove-colored-pieces-if-both-neighbors-are-the-same-color.cpp
+++ 2149-remove-colored-pieces-if-both-neighbors-are-the-same-color/2149-remove-colored-pieces-if-both-neighbors-are-the-same-color.cpp
@@ -1,13 +1,13 @@
 class Solution {
 public:
-    bool gameplay(int a, int b) {
-        // alice's turn
-        if (a <= 0) return false; // alice loses;
-        a--;
-        if (b <= 0) return true; // bob loses
-        b--;
-        return gameplay (a, b);
-    }
+    // bool gameplay(int a, int b) {
+    //     // alice's turn
+    //     if (a <= 0) return false; // alice loses;
+    //     a--;
+    //     if (b <= 0) return true; // bob loses
+    //     b--;
+    //     return gameplay (a, b);
+    // }
     bool winnerOfGame(string s) {
         if (s.size() < 3) return false;
         int a = 0;
@@ -16,6 +16,6 @@ public:
             if (s[i] == s[i-1] && s[i-1] == s[i-2] && s[i] == 'A') a++;
             if (s[i] == s[i-1] && s[i-1] == s[i-2] && s[i] == 'B') b++;
         }
-        return gameplay(a, b);
+        return a > b;
     }
 };
