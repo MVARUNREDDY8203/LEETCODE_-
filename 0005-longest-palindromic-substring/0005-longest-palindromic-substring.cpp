004977c99a2f3464ed534e13f3c7ed9acec6fb4a
M VARUN  REDDY <94187286+MVARUNREDDY8203@users.noreply.github.com>
2023-10-03 15:31:04 +0530

    Time: 810 ms (17.69%), Space: 23.3 MB (32.57%) - LeetHub

---
+++ 0005-longest-palindromic-substring/0005-longest-palindromic-substring.cpp
@@ -0,0 +1,67 @@
+class Solution {
+private: 
+    bool solve(vector<vector<bool>> &dp, int i, int j, string &s){
+        if(i == j){
+            return dp[i][j] = true;
+        }
+        if(j-i == 1){
+            if(s[i] == s[j]){
+                return dp[i][j] = true;
+            }
+            else{
+                return dp[i][j] = false;
+            }
+        }
+        if(s[i] == s[j] && dp[i+1][j-1] == true){
+            return dp[i][j] = true;
+        } else {
+            return dp[i][j] = false;
+        }
+    }
+public:
+    string longestPalindrome(string s) {
+        int n = s.size();
+        int startIndex = 0; int maxlen = 0;
+        vector<vector<bool>> dp(n, vector<bool>(n, false));
+        for(int g=0; g<n; g++){
+            for(int i=0, j=g; j<n; i++, j++){
+                solve(dp, i, j, s);
+                if(dp[i][j] == true){
+                    if(j-i+1 > maxlen){
+                        startIndex = i;
+                        maxlen = j-i+1;
+                    }
+                }
+            }
+        }
+        return s.substr(startIndex, maxlen);
+    }
+};
+// class Solution {
+// public:
+//     // string ans = "";
+//     int al =0;
+//     int ar =0;
+//     bool check_palindrome(string s, int l, int r, vector<vector<bool>> &dp) {
+//         if (l == r) return dp[l][r] = true;
+
+//         if (r-l == 1) return (s[l] == s[r] ? dp[l][r] = true: dp[l][r] = false);
+
+//         if (s[l] == s[r] && dp[l+1][r-1] == true) return dp[l][r] = true;
+//         return dp[l][r] = false;
+//     }
+//     string longestPalindrome(string s) {
+//         vector<vector<bool>> dp(s.size(), vector<bool>(s.size(), false));
+//         for (int len=0; len<s.size(); len++) {
+//             for (int i=0, j=len; j<s.size();i++, j++) {
+//                 if (check_palindrome(s, i, j, dp)){
+//                     if (ar-al+1 < j-i+1) {
+//                         al = i;
+//                         ar = j;
+//                     }
+//                 }
+//             }
+//         }
+//         return s.substr(al, ar-al+1);
+//     }
+// };
