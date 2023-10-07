179aef2322e7f4d9e0a534a338e866679e99934e
M VARUN  REDDY <94187286+MVARUNREDDY8203@users.noreply.github.com>
2023-10-07 12:32:51 +0530

    Time: 16 ms (7.33%), Space: 16.5 MB (9.1%) - LeetHub

--- 0229-majority-element-ii/0229-majority-element-ii.cpp
+++ 0229-majority-element-ii/0229-majority-element-ii.cpp
@@ -1,14 +1,35 @@
 class Solution {
 public:
     vector<int> majorityElement(vector<int>& nums) {
+        vector<int> ans;
+        sort(nums.begin(), nums.end());
         int n = nums.size();
         int n_by_3 = n/3;
-        unordered_map<int, int> ump;
-        for (auto i: nums) ump[i]++;
-        vector<int> ans;
-        for (auto i: ump) {
-            if (i.second > n_by_3) ans.emplace_back(i.first);
+        int prev = 1e9+1;
+
+        for (int i=0, j = n_by_3; i< n - n_by_3; i++, j++) {
+            if (nums[i] != prev && nums[i] == nums[j]) {
+                ans.emplace_back(nums[i]);
+                i = j;
+                j += n_by_3;
+                prev = nums[i];
+            }
         }
         return ans;
     }
 };
+
+// class Solution {
+// public:
+//     vector<int> majorityElement(vector<int>& nums) {
+//         int n = nums.size();
+//         int n_by_3 = n/3;
+//         unordered_map<int, int> ump;
+//         for (auto i: nums) ump[i]++;
+//         vector<int> ans;
+//         for (auto i: ump) {
+//             if (i.second > n_by_3) ans.emplace_back(i.first);
+//         }
+//         return ans;
+//     }
+// };
