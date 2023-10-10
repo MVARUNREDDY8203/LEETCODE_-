c6ace8c9cb85e7b66dfaad94db377b94099fad8b
M VARUN  REDDY <94187286+MVARUNREDDY8203@users.noreply.github.com>
2023-10-10 17:18:34 +0530

    Time: 13 ms (63.79%), Space: 19.8 MB (38.8%) - LeetHub

--- 0169-majority-element/0169-majority-element.cpp
+++ 0169-majority-element/0169-majority-element.cpp
@@ -1,10 +1,27 @@
 class Solution {
 public:
+    //--------------------BIT MANIPULATION-----------------------//
     int majorityElement(vector<int>& nums) {
-        sort(nums.begin(), nums.end());
-        return nums[nums.size()/2];
+        int ans = 0;
+        int n = nums.size();
+        for (int i=0; i<32; i++) {
+            int count = 0;
+            for (auto num: nums) {
+                if ((num & (1 << i)) != 0)  count ++; // AND with bitmask, checking if ith bit is set or not
+            }
+
+            if (count > n/2) ans |= (1 << i);  //setting the bit in the ans;
+        }
+        return ans;
     }
 };
+// class Solution {
+// public:
+//     int majorityElement(vector<int>& nums) {
+//         sort(nums.begin(), nums.end());
+//         return nums[nums.size()/2];
+//     }
+// };
 // class Solution {
 // public:
 //     int majorityElement(vector<int>& nums) {
