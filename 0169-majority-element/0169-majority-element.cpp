f1c70795e87521d09f9374eb3c8672f92f2abd83
M VARUN  REDDY <94187286+MVARUNREDDY8203@users.noreply.github.com>
2023-10-11 19:40:49 +0530

    Time: 14 ms (59.52%), Space: 19.8 MB (40.1%) - LeetHub

--- 0169-majority-element/0169-majority-element.cpp
+++ 0169-majority-element/0169-majority-element.cpp
@@ -1,20 +1,38 @@
 class Solution {
 public:
-    //--------------------BIT MANIPULATION-----------------------//
     int majorityElement(vector<int>& nums) {
-        int ans = 0;
-        int n = nums.size();
-        for (int i=0; i<32; i++) {
-            int count = 0;
-            for (auto num: nums) {
-                if ((num & (1 << i)) != 0)  count ++; // AND with bitmask, checking if ith bit is set or not
+        int majority = 0, count = 0;
+        for (int i=0; i<nums.size(); i++) {
+            if (count) {
+                count += (nums[i] == majority ? 1 : -1);
             }
-
-            if (count > n/2) ans |= (1 << i);  //setting the bit in the ans;
+            else{
+                majority = nums[i];
+                count = 1;
+            }
         }
-        return ans;
+        return majority;
     }
 };
+
+// class Solution {
+// public:
+//     //--------------------BIT MANIPULATION-----------------------//
+//     int majorityElement(vector<int>& nums) {
+//         int ans = 0;
+//         int n = nums.size();
+//         for (int i=0; i<32; i++) {
+//             int count = 0;
+//             for (auto num: nums) {
+//                 if ((num & (1 << i)) != 0)  count ++; // AND with bitmask, checking if ith bit is set or not
+//             }
+
+//             if (count > n/2) ans |= (1 << i);  //setting the bit in the ans;
+//         }
+//         return ans;
+//     }
+// };
+// --------------------- O(nlogn) time , O(1)space----------------------------------
 // class Solution {
 // public:
 //     int majorityElement(vector<int>& nums) {
@@ -22,6 +40,7 @@ public:
 //         return nums[nums.size()/2];
 //     }
 // };
+// -------------------- O(n) time, O(n) space --------------------------------------
 // class Solution {
 // public:
 //     int majorityElement(vector<int>& nums) {
