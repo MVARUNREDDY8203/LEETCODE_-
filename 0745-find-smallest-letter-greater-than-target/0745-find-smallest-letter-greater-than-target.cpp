8fe64e2c5bb01d88936fbd0a9b220c6ed80fe125
M VARUN  REDDY <94187286+MVARUNREDDY8203@users.noreply.github.com>
2023-10-19 11:56:42 +0530

    Time: 12 ms (53.46%), Space: 16.2 MB (28.86%) - LeetHub

--- 0745-find-smallest-letter-greater-than-target/0745-find-smallest-letter-greater-than-target.cpp
+++ 0745-find-smallest-letter-greater-than-target/0745-find-smallest-letter-greater-than-target.cpp
@@ -1,17 +1,25 @@
 class Solution {
 public:
-    char binarySearchSmn(vector<char> &letters, char target, int l, int r) {
-        if (l > r) return letters[0];
-        if (r - l <= 1) {
-            if (letters[l] > target) return letters[l];
-            if (letters[r] > target) return letters[r];
-            return letters[0];
-        }
-        int mid = (l+r)/2;
-        if (letters[mid] <= target) return binarySearchSmn(letters, target, mid+1, r);
-        return binarySearchSmn(letters, target, l, mid);
-    }
     char nextGreatestLetter(vector<char>& letters, char target) {
-        return binarySearchSmn(letters, target, 0, letters.size()-1);
+        int idx = upper_bound(letters.begin(), letters.end(), target) - letters.begin();
+        return idx >= letters.size() ? letters[0] : letters[idx];
     }
 };
+
+// class Solution {
+// public:
+//     char binarySearchSmn(vector<char> &letters, char target, int l, int r) {
+//         if (l > r) return letters[0];
+//         if (r - l <= 1) {
+//             if (letters[l] > target) return letters[l];
+//             if (letters[r] > target) return letters[r];
+//             return letters[0];
+//         }
+//         int mid = (l+r)/2;
+//         if (letters[mid] <= target) return binarySearchSmn(letters, target, mid+1, r);
+//         return binarySearchSmn(letters, target, l, mid);
+//     }
+//     char nextGreatestLetter(vector<char>& letters, char target) {
+//         return binarySearchSmn(letters, target, 0, letters.size()-1);
+//     }
+// };
