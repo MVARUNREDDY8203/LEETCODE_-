72ab89ea8c1b0bd6c0f1989fba6dade5cb134ece
M VARUN  REDDY <94187286+MVARUNREDDY8203@users.noreply.github.com>
2023-10-23 10:25:33 +0530

    Time: 41 ms (87.46%), Space: 48.7 MB (63.05%) - LeetHub

---
+++ 0055-jump-game/0055-jump-game.cpp
@@ -0,0 +1,39 @@
+class Solution {
+public:
+    bool canJump(vector<int> &nums) {
+        int min_jump = 0, n = nums.size();
+        for (int i=n-2; i>=0; i--) {
+            min_jump++;
+            if (nums[i] >= min_jump) min_jump = 0;
+        }
+        return min_jump == 0 ? true: false;
+    }
+};
+// checking for a zero (point of problem) and checking if it can be crossed or not O(n^2)
+// class Solution {
+// public:
+//     bool canJump(vector<int>& nums) {
+//         int size = nums.size();
+//         if(size == 1) return true;
+//         if(nums[0] == 0) return false;
+//         for(int i=0; i<size; i++){
+//             if(nums[i] == 0){
+//                 int j=i-1, count =1;
+//                 bool possible = false;
+//                 while(j >= 0){
+//                     if(nums[j]>count && i!=size-1){
+//                         possible = true;
+//                         break;
+//                     }else if(nums[j]>=count && i == size-1){
+//                         possible = true;
+//                         break;
+//                     }
+//                     count++;
+//                     j--;
+//                 }
+//                 if(!possible) return false;
+//             }
+//         }
+//         return true;
+//     }
+// };
