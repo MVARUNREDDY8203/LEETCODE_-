f594e793e9a07dc9a68697f63290464a74c0554f
M VARUN  REDDY <94187286+MVARUNREDDY8203@users.noreply.github.com>
2023-10-25 11:50:13 +0530

    Time: 90 ms (10.08%), Space: 35.6 MB (5.08%) - LeetHub

--- 0876-hand-of-straights/0876-hand-of-straights.cpp
+++ 0876-hand-of-straights/0876-hand-of-straights.cpp
@@ -3,23 +3,18 @@ public:
     bool isNStraightHand(vector<int>& hand, int groupSize) {
         int n = hand.size();
         if (n%groupSize != 0) return false;
-        // sort(hand.begin(), hand.end());
 
         unordered_map<int, int> ump;
         set<int> ust;
         unordered_map<int, int> ump2;
         for (int i=0; i<n; i++) {
             ust.insert(hand[i]);
-            // if (ump.find(hand[i] == ump.end())) ump[hand[i]] = i;
             ump2[hand[i]]++;
         }
         
         while (true) {
             if  (ust.empty()) return true;
             int value = *ust.begin();
-            // for (auto i: ust) cout<<i<<" ";
-            // cout<<endl;
-            cout<<value<<endl;
             for (int j = 1; j<groupSize; j++) {
                 if (ust.find(value+j) == ust.end() || ump2.find(value+j) == ump2.end() || ump2[value+j] <= 0) return false;
                 ump2[value+j]--;
@@ -31,8 +26,6 @@ public:
             if (ump2[value] <= 0) {
                 ust.erase(value);
             }
-            // for (auto i: ust) cout<<i<<" ";
-            // cout<<endl;
         }    
         return true;
     }
