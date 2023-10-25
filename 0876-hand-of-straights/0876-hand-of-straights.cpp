adb8978fe42a96f39da3b7bdf4634ff2eaf8fc1b
M VARUN  REDDY <94187286+MVARUNREDDY8203@users.noreply.github.com>
2023-10-25 12:46:10 +0530

    Time: 67 ms (50.88%), Space: 28.6 MB (38.55%) - LeetHub

--- 0876-hand-of-straights/0876-hand-of-straights.cpp
+++ 0876-hand-of-straights/0876-hand-of-straights.cpp
@@ -4,27 +4,54 @@ public:
         int n = hand.size();
         if (n%groupSize != 0) return false;
 
-        set<int> ust;
-        unordered_map<int, int> ump2;
+        map<int, int> ump2;
         for (int i=0; i<n; i++) {
-            ust.insert(hand[i]);
             ump2[hand[i]]++;
         }
         
         while (true) {
-            if (ust.empty()) return true;
-            int value = *ust.begin();
+            if (ump2.empty()) return true;
+            int value = ump2.begin()->first;
             for (int j = 1; j<groupSize; j++) {
-                if (ust.find(value+j) == ust.end() || ump2.find(value+j) == ump2.end() || ump2[value+j] <= 0) return false;
+                if (ump2.find(value+j) == ump2.end() || ump2[value+j] <= 0) return false;
                 ump2[value+j]--;
-                if (ump2[value+j] <= 0) ust.erase(value+j);
+                if (ump2[value+j] <= 0) ump2.erase(value+j);
             }
             ump2[value]--;
-            if (ump2[value] <= 0) ust.erase(value);
+            if (ump2[value] <= 0) ump2.erase(value);
         }    
         return true;
     }
 };
+
+
+// class Solution {
+// public:
+//     bool isNStraightHand(vector<int>& hand, int groupSize) {
+//         int n = hand.size();
+//         if (n%groupSize != 0) return false;
+
+//         set<int> ust;
+//         unordered_map<int, int> ump2;
+//         for (int i=0; i<n; i++) {
+//             ust.insert(hand[i]);
+//             ump2[hand[i]]++;
+//         }
+        
+//         while (true) {
+//             if (ust.empty()) return true;
+//             int value = *ust.begin();
+//             for (int j = 1; j<groupSize; j++) {
+//                 if (ust.find(value+j) == ust.end() || ump2.find(value+j) == ump2.end() || ump2[value+j] <= 0) return false;
+//                 ump2[value+j]--;
+//                 if (ump2[value+j] <= 0) ust.erase(value+j);
+//             }
+//             ump2[value]--;
+//             if (ump2[value] <= 0) ust.erase(value);
+//         }    
+//         return true;
+//     }
+// };
 
 
 // class Solution {
