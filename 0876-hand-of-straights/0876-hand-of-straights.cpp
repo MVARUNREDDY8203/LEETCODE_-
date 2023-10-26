9c00fef3dce39e813c75f27e411252a8220b1d80
M VARUN  REDDY <94187286+MVARUNREDDY8203@users.noreply.github.com>
2023-10-26 12:35:10 +0530

    Time: 104 ms (7.13%), Space: 28.8 MB (31.07%) - LeetHub

--- 0876-hand-of-straights/0876-hand-of-straights.cpp
+++ 0876-hand-of-straights/0876-hand-of-straights.cpp
@@ -1,28 +1,62 @@
 class Solution {
 public:
-    bool isNStraightHand(vector<int>& hand, int groupSize) {
+    bool isNStraightHand(vector<int>& hand, int gs) {
         int n = hand.size();
-        if (n%groupSize != 0) return false;
+        if (n % gs) return false;
+        priority_queue<int, vector<int>, greater<int>> pq;
+        unordered_map<int, int> ump;
+        for (int x: hand) ump[x]++;
+        for (auto it: ump) pq.push(it.first);
 
-        map<int, int> ump2;
-        for (int i=0; i<n; i++) {
-            ump2[hand[i]]++;
-        }
-        
-        while (true) {
-            if (ump2.empty()) return true;
-            int value = ump2.begin()->first;
-            for (int j = 1; j<groupSize; j++) {
-                if (ump2.find(value+j) == ump2.end()) return false;
-                ump2[value+j]--;
-                if (ump2[value+j] <= 0) ump2.erase(value+j);
+        while (!pq.empty()) {
+            int value = pq.top();
+            cout<<pq.top()<<endl;
+            if (ump.find(value) == ump.end()) {
+                pq.pop();
+                continue;
+            };
+            if (ump[value] <= 0) {
+                ump.erase(value);
+                continue;
             }
-            ump2[value]--;
-            if (ump2[value] <= 0) ump2.erase(value);
-        }    
+            ump[value]--;
+            for (int i = 1; i < gs; i++) {
+                if (ump.find(value + i) == ump.end()) return false;
+                ump[value+i]--;
+                if (ump[value+i] <= 0) ump.erase(value+i);
+            }
+        }
         return true;
     }
 };
+
+
+
+// class Solution {
+// public:
+//     bool isNStraightHand(vector<int>& hand, int groupSize) {
+//         int n = hand.size();
+//         if (n%groupSize != 0) return false;
+
+//         map<int, int> ump2;
+//         for (int i=0; i<n; i++) {
+//             ump2[hand[i]]++;
+//         }
+        
+//         while (true) {
+//             if (ump2.empty()) return true;
+//             int value = ump2.begin()->first;
+//             for (int j = 1; j<groupSize; j++) {
+//                 if (ump2.find(value+j) == ump2.end()) return false;
+//                 ump2[value+j]--;
+//                 if (ump2[value+j] <= 0) ump2.erase(value+j);
+//             }
+//             ump2[value]--;
+//             if (ump2[value] <= 0) ump2.erase(value);
+//         }    
+//         return true;
+//     }
+// };
 
 
 // class Solution {
