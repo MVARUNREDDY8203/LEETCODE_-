8876a895ef9f5ad59ac0e3f1a9418800eb7a3161
M VARUN  REDDY <94187286+MVARUNREDDY8203@users.noreply.github.com>
2023-10-27 20:55:30 +0530

    Time: 382 ms (86.92%), Space: 146.8 MB (50.05%) - LeetHub

--- 2026-merge-triplets-to-form-target-triplet/2026-merge-triplets-to-form-target-triplet.cpp
+++ 2026-merge-triplets-to-form-target-triplet/2026-merge-triplets-to-form-target-triplet.cpp
@@ -2,28 +2,12 @@ class Solution {
 public:
     bool mergeTriplets(vector<vector<int>>& triplets, vector<int>& target) {
         int one = target[0], two = target[1], three = target[2], n = triplets.size();
-        unordered_map<int, int>;
         bool possible1 = false, possible2 = false, possible3 = false;
         for (int i=0; i<n; i++) {
-            if (triplets[i][0] == one && triplets[i][1] <= two && triplets[i][2] <= three) {
-                possible1 = true;
-                break;
-            }
+            if (triplets[i][0] == one && triplets[i][1] <= two && triplets[i][2] <= three) possible1 = true;
+            if (triplets[i][0] <= one && triplets[i][1] == two && triplets[i][2] <= three) possible2 = true;
+            if (triplets[i][0] <= one && triplets[i][1] <= two && triplets[i][2] == three) possible3 = true;
         }
-        if (!possible1) return false;
-        for (int i=0; i<n; i++) {
-            if (triplets[i][0] <= one && triplets[i][1] == two && triplets[i][2] <= three) {
-                possible2 = true;
-                break;
-            }
-        }
-        if (!possible2) return false;
-        for (int i=0; i<n; i++) {
-            if (triplets[i][0] <= one && triplets[i][1] <= two && triplets[i][2] == three) {
-                possible3 = true;
-                break;
-            }
-        }
-        return possible3;
+        return possible1 && possible2 && possible3;
     }
 };
