24fee21bb3ef43c27017991e84edc63da83f2403
M VARUN  REDDY <94187286+MVARUNREDDY8203@users.noreply.github.com>
2023-10-28 14:55:28 +0530

    Time: 14 ms (5.28%), Space: 6.9 MB (74.66%) - LeetHub

---
+++ 0768-partition-labels/0768-partition-labels.cpp
@@ -0,0 +1,37 @@
+class Solution {
+public:
+    vector<int> partitionLabels(string s) {
+        int n = s.size(), ptr = 0, len = 0, end = -1, r = 0;
+        vector<int> ans;
+        unordered_set<int> ust;
+        while (ptr < n) {
+            if (ust.find(s[ptr]) != ust.end()) {
+                ptr++;
+                len++;
+                continue;
+            }
+            if (ptr == end) {
+                ans.emplace_back(++len);
+                len = 0;
+                ptr++;
+                // continue;
+            }
+            r = n-1;
+            while (r >= ptr) {
+                if (s[r] == s[ptr]) break;
+                r--;
+            }
+            if (r > end) end = r;
+            if (ptr == end) {
+                ans.emplace_back(++len);
+                len = 0;
+                ptr++;
+                continue;
+            }
+            ptr++;
+            len++;                        
+        }
+        // if (ust.find(s[--ptr]) == ust.end()) ans.push_back(len);
+        return ans;
+    }
+};
