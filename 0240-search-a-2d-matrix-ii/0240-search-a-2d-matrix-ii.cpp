1cb6f18a7dae2f6ee693dadb3f1d29dd3002d3cb
M VARUN  REDDY <94187286+MVARUNREDDY8203@users.noreply.github.com>
2023-11-01 22:45:36 +0530

    Time: 85 ms (67.7%), Space: 15.3 MB (12.08%) - LeetHub

--- 0240-search-a-2d-matrix-ii/0240-search-a-2d-matrix-ii.cpp
+++ 0240-search-a-2d-matrix-ii/0240-search-a-2d-matrix-ii.cpp
@@ -1,13 +1,27 @@
 class Solution {
 public:
     bool searchMatrix(vector<vector<int>>& matrix, int target) {
-        int m = matrix.size();
-        int n = matrix[0].size();
-        if (target < matrix[0][0] || target > matrix[m-1][n-1]) return false;
-        for (int i=0; i<m; i++) {
-            int idx = lower_bound(matrix[i].begin(), matrix[i].end(), target) - matrix[i].begin();
-            if (idx < n && matrix[i][idx] == target) return true;
-        } 
+        int m = matrix.size(), n = matrix[0].size();
+        int i = 0, j = n-1;
+        while (j >= 0 && i < m) {
+            if (target == matrix[i][j]) return true;
+            else if (target > matrix[i][j]) i++; // ans not in curr row cos row is sorted
+            else j--; // ans ant be in the col cos col is sorted
+        }   
         return false;
     }
 };
+
+// class Solution {
+// public:
+//     bool searchMatrix(vector<vector<int>>& matrix, int target) {
+//         int m = matrix.size();
+//         int n = matrix[0].size();
+//         if (target < matrix[0][0] || target > matrix[m-1][n-1]) return false;
+//         for (int i=0; i<m; i++) {
+//             int idx = lower_bound(matrix[i].begin(), matrix[i].end(), target) - matrix[i].begin();
+//             if (idx < n && matrix[i][idx] == target) return true;
+//         } 
+//         return false;
+//     }
+// };
