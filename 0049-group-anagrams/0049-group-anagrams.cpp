1d29700d8ef9bcce3b603d6f0d46e4ef28025183
M VARUN  REDDY <94187286+MVARUNREDDY8203@users.noreply.github.com>
2023-11-02 12:12:38 +0530

    Time: 41 ms (20.12%), Space: 21.3 MB (34.07%) - LeetHub

---
+++ 0049-group-anagrams/0049-group-anagrams.cpp
@@ -0,0 +1,89 @@
+class Solution {
+public:
+    vector<vector<string>> groupAnagrams(vector<string>& strs) {
+        // vector<string> strs2 = strs;
+        // vector<int>
+        map<string, vector<string>> mp;
+        for (auto i: strs) {
+            string s = i;
+            sort(s.begin(), s.end());
+            mp[s].push_back(i);
+        }
+        vector<vector<string>> ans;
+        for (auto it: mp) {
+            ans.push_back(it.second);
+        }
+        return ans;
+    }
+};
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// class Solution {
+// public:
+//     vector<vector<string>> groupAnagrams(vector<string>& s) {
+//         unordered_map<string, vector<string>> mp;
+
+//         for(int i=0; i<s.size(); i++){
+//             string temp = s[i];
+//             sort(temp.begin(), temp.end());
+
+//             mp[temp].push_back(s[i]);           
+//         }
+//         vector<vector<string>> ans;
+
+//         for(auto i: mp){
+//             ans.push_back(i.second);
+//         }
+
+        
+//         return ans;
+//         // unordered_map< unordered_map<char, int>, vector<string>> mp;
+        
+//         // for(int j=0; j<s.length(); j++){
+//         //     string str = s[j];
+//         //     unoredered_map<char, int> temp;
+//         //     for(int i=0; i<str.length(); i++){
+//         //         temp[str[i]]++;
+//         //     }
+            
+//         //     for(int k=0; k<mp.size(); k++){
+//         //         mp[temp].push_back(str);
+//         //     }	
+//         // }
+//         // vector<vector<string>> ans;
+//         // for(auto i: mp){
+//         //     ans.push_back(i.second);
+//         // }
+//         // return ans;
+//     }
+// };
