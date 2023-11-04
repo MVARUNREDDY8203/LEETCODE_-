7806b096b6a58c0ada1a028198f50d772b1793ad
M VARUN  REDDY <94187286+MVARUNREDDY8203@users.noreply.github.com>
2023-11-04 15:44:57 +0530

    Time: 34 ms (72%), Space: 19.3 MB (58.86%) - LeetHub

--- 0907-koko-eating-bananas/0907-koko-eating-bananas.cpp
+++ 0907-koko-eating-bananas/0907-koko-eating-bananas.cpp
@@ -3,25 +3,28 @@ public:
     int minEatingSpeed(vector<int>& piles, int h) {
         int max_val = -1;
         for (auto i: piles) max_val = max(max_val, i);
-        int l = 1, r = max_val;
-        while (r - l >= 10) {
+        int l = 1, r = max_val, ans = -1;
+        while (l <= r) {
             int mid = (l+r)/2;
             long long int time = 0;
             for (int j=0; j<piles.size(); j++) {
                 time += ceil(piles[j] / (double)mid);
             }
             // cout<<l<<" "<<mid<<" "<<r<<endl;
-            if (time > h) l = mid;
-            else r = mid;
-        }
-        for (int i=l; i<=r; i++) {
-            long long int time = 0;
-            for (int j=0; j<piles.size(); j++) {
-                time += ceil(piles[j] / (double)i);
+            if (time > h) l = mid+1;
+            else {
+                ans = mid;
+                r = mid-1;
             }
-            if (time <= h) return i;
         }
-        return l;
+        // for (int i=l; i<=r; i++) {
+        //     long long int time = 0;
+        //     for (int j=0; j<piles.size(); j++) {
+        //         time += ceil(piles[j] / (double)i);
+        //     }
+        //     if (time <= h) return i;
+        // }
+        return ans == -1 ? r : ans;
     }
 };
 /*
