a36a3bfad415482fef32b304195d50037bfc7723
M VARUN  REDDY <94187286+MVARUNREDDY8203@users.noreply.github.com>
2023-11-04 15:45:42 +0530

    Time: 34 ms (72%), Space: 19.2 MB (80.51%) - LeetHub

--- 0907-koko-eating-bananas/0907-koko-eating-bananas.cpp
+++ 0907-koko-eating-bananas/0907-koko-eating-bananas.cpp
@@ -7,31 +7,14 @@ public:
         while (l <= r) {
             int mid = (l+r)/2;
             long long int time = 0;
-            for (int j=0; j<piles.size(); j++) {
-                time += ceil(piles[j] / (double)mid);
-            }
-            // cout<<l<<" "<<mid<<" "<<r<<endl;
+            for (int j=0; j<piles.size(); j++) time += ceil(piles[j] / (double)mid);
+            
             if (time > h) l = mid+1;
             else {
                 ans = mid;
                 r = mid-1;
             }
         }
-        // for (int i=l; i<=r; i++) {
-        //     long long int time = 0;
-        //     for (int j=0; j<piles.size(); j++) {
-        //         time += ceil(piles[j] / (double)i);
-        //     }
-        //     if (time <= h) return i;
-        // }
         return ans == -1 ? r : ans;
     }
 };
-/*
-given :
-- array piles with piles[i] bananas;
-- h hrs
-- k bananas/hr
-- 1 hr choices only
-- 
-*/
