0503fa6e207c4c97b4d5d4aad0f5cda5b9f1f247
M VARUN  REDDY <94187286+MVARUNREDDY8203@users.noreply.github.com>
2024-02-23 12:14:09 +0530

    Time: 207 ms (47.78%) | Memory: 152.4 MB (18.75%) - LeetSync

---
+++ 239-sliding-window-maximum/sliding-window-maximum.cpp
@@ -0,0 +1,60 @@
+class Solution {
+public:
+    vector<int> maxSlidingWindow(vector<int>& nums, int k) {
+        priority_queue<pair<int, int>> max_heap;
+        for (int i=0; i<k; i++) { 
+            max_heap.push({nums[i], i});
+        }
+        vector<int> ans;
+        ans.push_back(max_heap.top().first);
+        for (int i=k; i<nums.size(); i++) {
+            max_heap.push({nums[i], i});
+            while (max_heap.top().second <= i-k) {
+                max_heap.pop();
+            }
+            ans.push_back(max_heap.top().first);
+        }
+        return ans;
+    }
+};
+
+
+// class Solution {
+// public:
+//     vector<int> maxSlidingWindow(vector<int>& nums, int k) {
+//         deque<int> dq;
+//         vector<int> ans;
+
+//         for(int i=0; i<nums.size(); i++){
+//             if(!dq.empty() && dq.front() == i-k) {  // sliding window boundary condition check
+//                 dq.pop_front();
+//             }
+
+        
+//             while(!dq.empty() && nums[dq.back()] < nums[i]){
+//                 dq.pop_back();
+//             }
+
+//             dq.push_back(i);
+//             if(i>=k-1) ans.push_back(nums[dq.front()]);
+//         }  
+//         return ans;
+//     }
+// };
+
+// naive brute force == TLE
+// vector<int> maxSlidingWindow(vector<int>& nums, int k) {
+//     int size = nums.size();
+//     if(size == 1) return nums;
+//     vector<int> ans(size-k+1);
+//     if(size == k){
+//         for(int i=0; i<x)
+//     }
+//     for(int i=0; i<=size-k; i++){
+//         ans[i] = nums[i];
+//         for(int j=i; j<i+k; j++){
+//             ans[i] = max(ans[i], nums[j]);
+//         }
+//     }
+//     return ans;
+// }
