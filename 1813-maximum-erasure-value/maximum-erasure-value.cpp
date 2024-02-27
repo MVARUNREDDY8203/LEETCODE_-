25282c4ff29a68d34a5ac8ff327e2c843b73645d
M VARUN  REDDY <94187286+MVARUNREDDY8203@users.noreply.github.com>
2024-02-27 22:00:15 +0530

    Time: 115 ms (97.19%) | Memory: 94.5 MB (95.58%) - LeetSync

--- 1813-maximum-erasure-value/maximum-erasure-value.cpp
+++ 1813-maximum-erasure-value/maximum-erasure-value.cpp
@@ -1,22 +1,40 @@
 class Solution {
 public:
     int maximumUniqueSubarray(vector<int>& nums) {
+        vector<int> dict(10001, 0);
         int start = 0, end = 0, ans = 0, sum = 0;
-        unordered_set<int> dict;
         while (end < nums.size()) {
-            // cout<<start<<" "<<end<<endl;
-            if (dict.count(nums[end])) {
-                while (dict.count(nums[end])) {
-                    sum -= nums[start];
-                    dict.erase(nums[start++]);
-                }
+            while (dict[nums[end]] > 0) {
+                sum -= nums[start];
+                dict[nums[start++]]--;
             }
-            else {
-                sum += nums[end];
-                dict.insert(nums[end++]);
-            }
+            sum += nums[end];
+            dict[nums[end++]]++;
             ans = max(ans, sum);
         }
         return ans;
     }
 };
+
+
+// class Solution {
+// public:
+//     int maximumUniqueSubarray(vector<int>& nums) {
+//         int start = 0, end = 0, ans = 0, sum = 0;
+//         unordered_set<int> dict;
+//         while (end < nums.size()) {
+//             if (dict.count(nums[end])) {
+//                 while (dict.count(nums[end])) {
+//                     sum -= nums[start];
+//                     dict.erase(nums[start++]);
+//                 }
+//             }
+//             else {
+//                 sum += nums[end];
+//                 dict.insert(nums[end++]);
+//             }
+//             ans = max(ans, sum);
+//         }
+//         return ans;
+//     }
+// };
