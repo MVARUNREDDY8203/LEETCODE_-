033f173fd031ea7fb03d091b5016b3c8da7996bf
M VARUN  REDDY <94187286+MVARUNREDDY8203@users.noreply.github.com>
2024-03-07 11:34:22 +0530

    Time: 0 ms (100.00%) | Memory: 9.8 MB (26.41%) - LeetSync

--- 75-sort-colors/sort-colors.cpp
+++ 75-sort-colors/sort-colors.cpp
@@ -1,20 +1,35 @@
 class Solution {
 public:
     void sortColors(vector<int>& nums) {
-        int l = 0, n = nums.size(), l2 = n-1;
-        while (l < n && l <= l2) {
-            while (l < n && l < l2 && nums[l] == 0) l++;
-            while (l2 > l && l2 >= 0 && nums[l2] != 0) l2--;
-            swap (nums[l++], nums[l2--]);
+        int zeros = 0, ones = 0, twos = 0, n = nums.size();
+        for (int i=0; i<n; i++) {
+            if (nums[i] == 0) zeros++;
+            else if (nums[i] == 1) ones++;
+            else twos++;
         }
-
-        l = 0;
-        while (l < n && nums[l] == 0) l++;
-        l2 = n-1;
-        while (l < l2 && l < n && l2 >= 0) {
-            while (l < n && l < l2 && nums[l] == 1) l++;
-            while (l2 >= 0 && l2 >= l && nums[l2] != 1) l2--;
-            if (l < l2)swap (nums[l++], nums[l2--]);
-        }
+        for (int i=0; i<zeros; i++) nums[i] = 0;
+        for (int j=zeros; j<ones+zeros; j++) nums[j] = 1;
+        for (int j=ones+zeros; j<n; j++) nums[j] = 2;
     }
 };
+
+// class Solution {
+// public:
+//     void sortColors(vector<int>& nums) {
+//         int l = 0, n = nums.size(), l2 = n-1;
+//         while (l < n && l <= l2) {
+//             while (l < n && l < l2 && nums[l] == 0) l++;
+//             while (l2 > l && l2 >= 0 && nums[l2] != 0) l2--;
+//             swap (nums[l++], nums[l2--]);
+//         }
+
+//         l = 0;
+//         while (l < n && nums[l] == 0) l++;
+//         l2 = n-1;
+//         while (l < l2 && l < n && l2 >= 0) {
+//             while (l < n && l < l2 && nums[l] == 1) l++;
+//             while (l2 >= 0 && l2 >= l && nums[l2] != 1) l2--;
+//             if (l < l2)swap (nums[l++], nums[l2--]);
+//         }
+//     }
+// };
