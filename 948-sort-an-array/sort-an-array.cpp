11cd8454813752ff99b4ab0a8ed80a71ed9aea7d
M VARUN  REDDY <94187286+MVARUNREDDY8203@users.noreply.github.com>
2024-03-07 09:57:09 +0530

    Time: 89 ms (92.19%) | Memory: 70.2 MB (65.73%) - LeetSync

--- 948-sort-an-array/sort-an-array.cpp
+++ 948-sort-an-array/sort-an-array.cpp
@@ -1,27 +1,55 @@
 class Solution {
 public:
-    void mergeSort(vector<int> &nums, int l, int r) {
-        if (r <= l) return;
+    // void mergeSort(vector<int> &nums, int l, int r) {
+    //     if (r <= l) return;
 
-        int mid = (r+l)/2;
-        mergeSort(nums, l, mid);
-        mergeSort(nums, mid+1, r);
+    //     int mid = (r+l)/2;
+    //     mergeSort(nums, l, mid);
+    //     mergeSort(nums, mid+1, r);
 
-        vector<int> arr3;
-        int i=l, j=mid+1;
-        while (i <= mid && j <= r) {
-            arr3.emplace_back(nums[i] < nums[j] ? nums[i++] : nums[j++]);
-        }
-        while (i <= mid) arr3.emplace_back(nums[i++]);
-        while (j <= r) arr3.emplace_back(nums[j++]);
+    //     vector<int> arr3;
+    //     int i=l, j=mid+1;
+    //     while (i <= mid && j <= r) {
+    //         arr3.emplace_back(nums[i] < nums[j] ? nums[i++] : nums[j++]);
+    //     }
+    //     while (i <= mid) arr3.emplace_back(nums[i++]);
+    //     while (j <= r) arr3.emplace_back(nums[j++]);
         
-        int l2 = l;
-        for (int i=0; i<arr3.size(); i++) {
-            nums[l2++] = arr3[i];
-        }
-    }
+    //     int l2 = l;
+    //     for (int i=0; i<arr3.size(); i++) {
+    //         nums[l2++] = arr3[i];
+    //     }
+    // }
     vector<int> sortArray(vector<int>& nums) {
-        mergeSort(nums, 0, nums.size()-1);
+        // mergeSort(nums, 0, nums.size()-1);
+        sort(nums.begin(), nums.end());
         return nums;
     }
 };
+// class Solution {
+// public:
+//     void mergeSort(vector<int> &nums, int l, int r) {
+//         if (r <= l) return;
+
+//         int mid = (r+l)/2;
+//         mergeSort(nums, l, mid);
+//         mergeSort(nums, mid+1, r);
+
+//         vector<int> arr3;
+//         int i=l, j=mid+1;
+//         while (i <= mid && j <= r) {
+//             arr3.emplace_back(nums[i] < nums[j] ? nums[i++] : nums[j++]);
+//         }
+//         while (i <= mid) arr3.emplace_back(nums[i++]);
+//         while (j <= r) arr3.emplace_back(nums[j++]);
+        
+//         int l2 = l;
+//         for (int i=0; i<arr3.size(); i++) {
+//             nums[l2++] = arr3[i];
+//         }
+//     }
+//     vector<int> sortArray(vector<int>& nums) {
+//         mergeSort(nums, 0, nums.size()-1);
+//         return nums;
+//     }
+// };
