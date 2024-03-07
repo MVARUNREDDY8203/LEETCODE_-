754fea4a18673bee33242c6ceb9bef024cdd7e78
M VARUN  REDDY <94187286+MVARUNREDDY8203@users.noreply.github.com>
2024-03-07 22:16:46 +0530

    Time: 3 ms (41.27%) | Memory: 8.7 MB (7.87%) - LeetSync

---
+++ 908-middle-of-the-linked-list/middle-of-the-linked-list.cpp
@@ -0,0 +1,33 @@
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode() : val(0), next(nullptr) {}
+ *     ListNode(int x) : val(x), next(nullptr) {}
+ *     ListNode(int x, ListNode *next) : val(x), next(next) {}
+ * };
+ */
+class Solution {
+public:
+    //tortoise and hare algo
+    ListNode* middleNode(ListNode* head){
+        ListNode* slow = head;
+        ListNode* fast = head;
+        while(fast && fast->next){
+            fast = fast->next->next;
+            slow = slow->next;
+        }
+        return slow;
+    }
+    // int total_nodes(ListNode* head){
+    //     return head ? 1 + total_nodes(head->next) : 0;
+    // }
+    // ListNode* mN_util(ListNode* head, int rem){
+    //     return (rem > 0) ? mN_util(head->next , rem-1) : head; 
+    // }
+    // ListNode* middleNode(ListNode* head) {
+    //     int tn = total_nodes(head);
+    //     return mN_util(head, tn/2);
+    // }
+};
