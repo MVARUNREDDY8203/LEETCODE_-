fb5cf03336e3e56e36be16aae08bd07add41bc29
M VARUN  REDDY <94187286+MVARUNREDDY8203@users.noreply.github.com>
2024-03-10 09:54:10 +0530

    Time: 1720 ms (16.69%) | Memory: 34.2 MB (81.19%) - LeetSync

--- 560-subarray-sum-equals-k/subarray-sum-equals-k.cpp
+++ 560-subarray-sum-equals-k/subarray-sum-equals-k.cpp
@@ -2,13 +2,39 @@ class Solution {
 public:
     int subarraySum(vector<int>& nums, int k) {
         int ans = 0, n = nums.size();
-        for (int i=0; i<n; i++) {
+
+        vector<int> sum_bw_0_and(n, 0);
+        sum_bw_0_and[0] = nums[0];
+        if (nums[0] == k) ans++;
+        for (int i=1; i<n; i++) {
+            sum_bw_0_and[i] = sum_bw_0_and[i-1] + nums[i];
+            if (sum_bw_0_and[i] == k) ans++;
+        }
+
+        
+        for (int i=1; i<n; i++) {
             int sum = 0;
             for (int j=i; j<n; j++) {
-                sum += nums[j];
+                sum = sum_bw_0_and[j] - sum_bw_0_and[i-1];
                 if (sum == k) ans++;
             }
         }
         return ans;
     }
 };
+
+
+// class Solution {
+// public:
+//     int subarraySum(vector<int>& nums, int k) {
+//         int ans = 0, n = nums.size();
+//         for (int i=0; i<n; i++) {
+//             int sum = 0;
+//             for (int j=i; j<n; j++) {
+//                 sum += nums[j];
+//                 if (sum == k) ans++;
+//             }
+//         }
+//         return ans;
+//     }
+// };
