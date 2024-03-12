e3e14bb60aafb7dd792614976aeb818665229f16
M VARUN  REDDY <94187286+MVARUNREDDY8203@users.noreply.github.com>
2024-03-12 21:26:20 +0530

    Time: 9 ms (59.44%) | Memory: 18 MB (17.07%) - LeetSync

--- 167-two-sum-ii-input-array-is-sorted/two-sum-ii-input-array-is-sorted.cpp
+++ 167-two-sum-ii-input-array-is-sorted/two-sum-ii-input-array-is-sorted.cpp
@@ -1,14 +1,31 @@
 class Solution {
 public:
     vector<int> twoSum(vector<int>& nums, int target) {
-        unordered_map<int, int> ump;
-        for (int i=0; i<nums.size(); i++) {
-            if (ump.count(target - nums[i])) return {ump[target - nums[i]] + 1, i + 1};
-            ump[nums[i]] = i;
+        int l = 0, r = nums.size() - 1, sum = nums[l] + nums[r];
+        while (l < r) {
+            if (sum > target) {
+                sum = nums[l] + nums[--r];
+            }
+            else if (sum < target) {
+                sum = nums[++l] + nums[r];
+            }
+            else return {l + 1, r + 1};
         }
-        return {0, 0};
+        return {l+1, r+1};
     }
 };
+
+// class Solution {
+// public:
+//     vector<int> twoSum(vector<int>& nums, int target) {
+//         unordered_map<int, int> ump;
+//         for (int i=0; i<nums.size(); i++) {
+//             if (ump.count(target - nums[i])) return {ump[target - nums[i]] + 1, i + 1};
+//             ump[nums[i]] = i;
+//         }
+//         return {0, 0};
+//     }
+// };
 
 
 
