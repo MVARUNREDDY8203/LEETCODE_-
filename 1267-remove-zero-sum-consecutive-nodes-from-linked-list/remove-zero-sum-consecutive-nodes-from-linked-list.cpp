c0faa343624a7e227f5c66fdd9c899510ebf1742
M VARUN  REDDY <94187286+MVARUNREDDY8203@users.noreply.github.com>
2024-03-13 21:30:28 +0530

    Time: 4 ms (90.44%) | Memory: 13.5 MB (66.33%) - LeetSync

---
+++ 1267-remove-zero-sum-consecutive-nodes-from-linked-list/remove-zero-sum-consecutive-nodes-from-linked-list.cpp
@@ -0,0 +1,51 @@
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode() : val(0), next(nullptr) {}
+ *     ListNode(int x) : val(x), next(nullptr) {}
+ *     ListNode(int x, ListNode *next) : val(x), next(next) {}
+ * };
+ */
+class Solution {
+public:
+    ListNode* removeZeroSumSublists(ListNode* head) {
+        vector<int> nums;
+        ListNode* curr = head;
+        while (curr) {
+            nums.push_back(curr->val);
+            curr = curr->next;
+        }
+
+        for (int i=0; i<nums.size(); i++) {
+            if (nums[i] == 2000) continue;
+
+            int sum = 0;
+            for (int j=i; j<nums.size(); j++) {
+
+                sum += nums[j];
+                if (sum == 0) {
+                    for (int k=i; k<=j; k++) {
+                        nums[k] = 2000;
+                    }
+                    // break;
+                    i = j;
+                }
+            }
+        }
+        
+        ListNode* head2 = new ListNode(2000);
+        curr = head2;
+        int m = 0;
+        while (m < nums.size()) {
+            if (nums[m] == 2000) {
+                m++;
+                continue;
+            }
+            curr->next = new ListNode(nums[m++]);
+            curr = curr->next;
+        }
+        return head2->next;
+    }
+};
