df383ad473d033043c5504d64a2e04b9d1e79cae
M VARUN  REDDY <94187286+MVARUNREDDY8203@users.noreply.github.com>
2024-03-13 22:16:26 +0530

    Time: 28 ms (5.42%) | Memory: 14.9 MB (37.23%) - LeetSync

--- 1267-remove-zero-sum-consecutive-nodes-from-linked-list/remove-zero-sum-consecutive-nodes-from-linked-list.cpp
+++ 1267-remove-zero-sum-consecutive-nodes-from-linked-list/remove-zero-sum-consecutive-nodes-from-linked-list.cpp
@@ -8,43 +8,79 @@
  *     ListNode(int x, ListNode *next) : val(x), next(next) {}
  * };
  */
+
+
 class Solution {
 public:
     ListNode* removeZeroSumSublists(ListNode* head) {
-        vector<int> nums;
+        unordered_map<int, ListNode*> ump;
         ListNode* curr = head;
+        ListNode* head2 = new ListNode(2000, head);
+
+        int sum = 0;
+        ump[0] = head2;
         while (curr) {
-            nums.push_back(curr->val);
-            curr = curr->next;
-        }
-
-        for (int i=0; i<nums.size(); i++) {
-            if (nums[i] == 2000) continue;
-
-            int sum = 0;
-            for (int j=i; j<nums.size(); j++) {
-
-                sum += nums[j];
-                if (sum == 0) {
-                    for (int k=i; k<=j; k++) {
-                        nums[k] = 2000;
-                    }
-                    i = j;
+            sum += curr->val;
+            cout<<sum<<" ";
+            if (ump.count(sum)) {
+                curr = ump[sum]->next;
+                int p = sum + curr->val;
+                while (p != sum) {
+                    ump.erase(p);
+                    curr = curr->next;
+                    p += curr->val;
                 }
+                ump[sum]->next = curr->next;
             }
-        }
-        
-        ListNode* head2 = new ListNode(2000);
-        curr = head2;
-        int m = 0;
-        while (m < nums.size()) {
-            if (nums[m] == 2000) {
-                m++;
-                continue;
-            }
-            curr->next = new ListNode(nums[m++]);
+            else ump[sum] = curr;
+            cout<<endl;
             curr = curr->next;
         }
+
         return head2->next;
+
     }
 };
+
+
+
+// class Solution {
+// public:
+//     ListNode* removeZeroSumSublists(ListNode* head) {
+//         vector<int> nums;
+//         ListNode* curr = head;
+//         while (curr) {
+//             nums.push_back(curr->val);
+//             curr = curr->next;
+//         }
+
+//         for (int i=0; i<nums.size(); i++) {
+//             if (nums[i] == 2000) continue;
+
+//             int sum = 0;
+//             for (int j=i; j<nums.size(); j++) {
+
+//                 sum += nums[j];
+//                 if (sum == 0) {
+//                     for (int k=i; k<=j; k++) {
+//                         nums[k] = 2000;
+//                     }
+//                     i = j;
+//                 }
+//             }
+//         }
+        
+//         ListNode* head2 = new ListNode(2000);
+//         curr = head2;
+//         int m = 0;
+//         while (m < nums.size()) {
+//             if (nums[m] == 2000) {
+//                 m++;
+//                 continue;
+//             }
+//             curr->next = new ListNode(nums[m++]);
+//             curr = curr->next;
+//         }
+//         return head2->next;
+//     }
+// };
