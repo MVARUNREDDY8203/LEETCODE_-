244d29041d4a4fa0f731ddfee27235a77ed80ac6
M VARUN  REDDY <94187286+MVARUNREDDY8203@users.noreply.github.com>
2024-03-21 21:15:07 +0530

    Time: 131 ms (82.84%) | Memory: 125.7 MB (84.69%) - LeetSync

--- 2085-array-with-elements-not-equal-to-average-of-neighbors/array-with-elements-not-equal-to-average-of-neighbors.cpp
+++ 2085-array-with-elements-not-equal-to-average-of-neighbors/array-with-elements-not-equal-to-average-of-neighbors.cpp
@@ -1,12 +1,26 @@
 class Solution {
 public:
     vector<int> rearrangeArray(vector<int>& nums) {
-        sort(nums.begin(), nums.end());
-        int l = 1, n = nums.size();
-        while (l < n) {
-            swap(nums[l], nums[l-1]);
-            l += 2;
+        for (int i=1; i<nums.size()-1; i++) {
+            if (nums[i]*2 == nums[i-1] + nums[i+1]) swap(nums[i], nums[i+1]);
+        }
+
+        for (int i=nums.size()-2; i>=1; i--) {
+            if (nums[i]*2 == nums[i-1] + nums[i+1]) swap(nums[i], nums[i-1]);
         }
         return nums;
     }
 };
+
+// class Solution {
+// public:
+//     vector<int> rearrangeArray(vector<int>& nums) {
+//         sort(nums.begin(), nums.end());
+//         int l = 1, n = nums.size();
+//         while (l < n) {
+//             swap(nums[l], nums[l-1]);
+//             l += 2;
+//         }
+//         return nums;
+//     }
+// };
