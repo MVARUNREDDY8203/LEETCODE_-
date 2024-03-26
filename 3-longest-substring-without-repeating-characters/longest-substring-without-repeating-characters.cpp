8ecff8cdabf1070ad3b029f511252cb8502f6148
M VARUN  REDDY <94187286+MVARUNREDDY8203@users.noreply.github.com>
2024-03-26 22:52:19 +0530

    Time: 19 ms (44.40%) | Memory: 11.4 MB (50.84%) - LeetSync

---
+++ 3-longest-substring-without-repeating-characters/longest-substring-without-repeating-characters.cpp
@@ -0,0 +1,41 @@
+class Solution {
+public:
+    int lengthOfLongestSubstring(string s) {
+        unordered_map<int, int> ump;
+        int start = 0, e = 0, n = s.size(), ans = 0;
+        while (e < n) {
+            ump[s[e]]++;
+            while (ump[s[e]] > 1) {
+                ump[s[start++]]--;
+            }
+            ans = max(ans, e - start +1);
+            e++;
+        }
+        return ans;
+    }
+};
+
+
+
+
+
+
+
+
+
+// class Solution {
+// public:
+//     int lengthOfLongestSubstring(string s) {
+//         vector<int> dict(256, -1);
+//         int start = -1, maxLen =0;
+//         for(int i=0; i<s.length(); i++){
+//             if(dict[s[i]] > start){
+//                 start = dict[s[i]];
+//             }
+
+//             dict[s[i]] = i;
+//             maxLen = max(maxLen, i-start);
+//         }
+//         return maxLen;
+//     }
+// };
