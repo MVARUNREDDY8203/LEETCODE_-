3917b0f78b779e50d390fe919b8cc8efbd2fd5c4
M VARUN  REDDY <94187286+MVARUNREDDY8203@users.noreply.github.com>
2024-03-28 19:56:29 +0530

    Time: 21 ms (76.78%) | Memory: 21.9 MB (21.56%) - LeetSync

--- 235-lowest-common-ancestor-of-a-binary-search-tree/lowest-common-ancestor-of-a-binary-search-tree.cpp
+++ 235-lowest-common-ancestor-of-a-binary-search-tree/lowest-common-ancestor-of-a-binary-search-tree.cpp
@@ -10,34 +10,48 @@
 
 class Solution {
 public:
-    bool find(TreeNode* root, int p) {
-        if (!root) return false;
-
-        if (root->val == p) return true;
-        if (root->val < p) return find(root->right, p);
-        return find(root->left, p);
-    }
     TreeNode* lowestCommonAncestor(TreeNode* root, TreeNode* p, TreeNode* q) {
-        int pp = p->val, qq = q->val;
-
-        queue<TreeNode*> que;
-        que.push(root);
-
-        TreeNode* ans = NULL;
-        // int deepest_lvl = 0;
-
-        while (!que.empty()) {
-            TreeNode* curr = que.front();
-            // int lvl = que.front().second;
-            que.pop();
-
-            if (find(curr, pp) && find(curr, qq)) {
-                ans = curr;
-                que.push(curr->left);
-                que.push(curr->right);
-            }
+        while (root) {
+            if (root->val > p->val && root->val > q->val) root = root->left;
+            else if (root->val < p->val && root->val < q->val) root = root->right;
+            else return root;
         }
-
-        return ans;
+        return NULL;
     }
 };
+
+
+
+// class Solution {
+// public:
+//     bool find(TreeNode* root, int p) {
+//         if (!root) return false;
+
+//         if (root->val == p) return true;
+//         if (root->val < p) return find(root->right, p);
+//         return find(root->left, p);
+//     }
+//     TreeNode* lowestCommonAncestor(TreeNode* root, TreeNode* p, TreeNode* q) {
+//         int pp = p->val, qq = q->val;
+
+//         queue<TreeNode*> que;
+//         que.push(root);
+
+//         TreeNode* ans = NULL;
+//         // int deepest_lvl = 0;
+
+//         while (!que.empty()) {
+//             TreeNode* curr = que.front();
+//             // int lvl = que.front().second;
+//             que.pop();
+
+//             if (find(curr, pp) && find(curr, qq)) {
+//                 ans = curr;
+//                 que.push(curr->left);
+//                 que.push(curr->right);
+//             }
+//         }
+
+//         return ans;
+//     }
+// };
