668b4be8fa8febaa1d3f82ecb311cf11c7f276b3
M VARUN  REDDY <94187286+MVARUNREDDY8203@users.noreply.github.com>
2024-03-29 22:38:27 +0530

    Time: 231 ms (10.21%) | Memory: 101.7 MB (5.02%) - LeetSync

--- 1117-as-far-from-land-as-possible/as-far-from-land-as-possible.cpp
+++ 1117-as-far-from-land-as-possible/as-far-from-land-as-possible.cpp
@@ -3,19 +3,25 @@ private:
 public:
     queue<vector<int>> bfs_que;
 
-    void bfs(vector<vector<int>>&graph) {
+    int maxDistance(vector<vector<int>>& graph) {
+        ios_base::sync_with_stdio(false);cin.tie(nullptr);cout.tie(nullptr);
         int n = graph.size();
+
+        int ans = -1, land_cnt = 0, water_cnt = 0;
 
         for (int i=0; i<n; i++) {
             for (int j=0; j<n; j++) {
                 if (graph[i][j] == 1) {
+                    land_cnt++;
                     bfs_que.push({i-1, j, 2});
                     bfs_que.push({i+1, j, 2});
                     bfs_que.push({i, j+1, 2});
                     bfs_que.push({i, j-1, 2});
-                } 
+                } else water_cnt++;
             }
         }
+
+        if (!land_cnt || !water_cnt) return -1;
 
         while (!bfs_que.empty()) {
             int i = bfs_que.front()[0];
@@ -26,6 +32,7 @@ public:
             if (i < 0 || j < 0 || i >=n || j >= n) continue;
 
             if (graph[i][j] == 0) {
+                ans = max(ans, dist);
                 graph[i][j] = dist;
 
                 bfs_que.push({i-1, j, dist+1});
@@ -34,28 +41,12 @@ public:
                 bfs_que.push({i, j+1, dist+1});
             }
         }
-    }
-    int maxDistance(vector<vector<int>>& graph) {
-        ios_base::sync_with_stdio(false);cin.tie(nullptr);cout.tie(nullptr);
-        int n = graph.size();
 
-        int ans = -1, land_cnt = 0, water_cnt = 0;
-        for (int i=0; i<n; i++) {
-            for (int j=0; j<n; j++) {
-                if (graph[i][j] == 1) {
-                    land_cnt++;
-                }
-                else water_cnt++;
-            }
-        }
-        if (!land_cnt || !water_cnt) return -1;
-
-        bfs(graph);
-        for (int i=0; i<n; i++) {
-            for (int j=0; j<n; j++) {
-                ans = max(ans, graph[i][j]);
-            }
-        }
+        // for (int i=0; i<n; i++) {
+        //     for (int j=0; j<n; j++) {
+        //         ans = max(ans, graph[i][j]);
+        //     }
+        // }
         return ans-1;
     }
 };
