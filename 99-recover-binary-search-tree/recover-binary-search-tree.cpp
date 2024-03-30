4590908a92d92928794740b2983398e9467b7c06
M VARUN  REDDY <94187286+MVARUNREDDY8203@users.noreply.github.com>
2024-03-30 16:01:45 +0530

    Time: 33 ms (7.38%) | Memory: 60.2 MB (90.60%) - LeetSync

--- 99-recover-binary-search-tree/recover-binary-search-tree.cpp
+++ 99-recover-binary-search-tree/recover-binary-search-tree.cpp
@@ -1,33 +1,78 @@
-/**
- * Definition for a binary tree node.
- * struct TreeNode {
- *     int val;
- *     TreeNode *left;
- *     TreeNode *right;
- *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
- *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
- *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
- * };
- */
+// /**
+//  * Definition for a binary tree node.
+//  * struct TreeNode {
+//  *     int val;
+//  *     TreeNode *left;
+//  *     TreeNode *right;
+//  *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+//  *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+//  *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+//  * };
+//  */
+
 class Solution {
 public:
-    void corrector(TreeNode* root, vector<int> &vec, int &i) {
+    TreeNode* first = NULL;
+    TreeNode* second = NULL;
+    TreeNode* prev = NULL;
+    void inorder(TreeNode* root) {
         if (!root) return;
-        corrector(root->left, vec, i);
-        root->val = vec[i++];
-        corrector(root->right, vec, i);
-    }
-    void traversal_lnr(TreeNode* root, vector<int> &vec) {
-        if (!root) return;
-        traversal_lnr(root->left, vec);
-        vec.push_back(root->val);
-        traversal_lnr(root->right, vec);
+        inorder(root->left);
+        if (!prev) {
+            prev = root;
+        }
+        if (root->val < prev->val && !first) {
+            first = prev;
+            second = root;
+        } 
+        if (root->val < prev->val && first) {
+            second = root;
+        }
+        prev = root;
+        inorder(root->right);
     }
     void recoverTree(TreeNode* root) {
-        vector<int> vec;
-        traversal_lnr(root, vec);
-        sort(vec.begin(), vec.end());
-        int i=0;
-        corrector(root, vec, i);
+        
+        inorder(root);
+        int p1 = first->val;
+        int p2 = second->val;
+        first->val = p2;
+        second->val = p1;
     }
 };
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// class Solution {
+// public:
+//     void corrector(TreeNode* root, vector<int> &vec, int &i) {
+//         if (!root) return;
+//         corrector(root->left, vec, i);
+//         root->val = vec[i++];
+//         corrector(root->right, vec, i);
+//     }
+//     void traversal_lnr(TreeNode* root, vector<int> &vec) {
+//         if (!root) return;
+//         traversal_lnr(root->left, vec);
+//         vec.push_back(root->val);
+//         traversal_lnr(root->right, vec);
+//     }
+//     void recoverTree(TreeNode* root) {
+//         vector<int> vec;
+//         traversal_lnr(root, vec);
+//         sort(vec.begin(), vec.end());
+//         int i=0;
+//         corrector(root, vec, i);
+//     }
+// };
