a1787fe37589f9d8e3c6b83905fee2facc049db0
M VARUN  REDDY <94187286+MVARUNREDDY8203@users.noreply.github.com>
2024-03-30 10:33:33 +0530

    Time: 91 ms (24.43%) | Memory: 17.1 MB (44.32%) - LeetSync

---
+++ 149-max-points-on-a-line/max-points-on-a-line.cpp
@@ -0,0 +1,22 @@
+class Solution {
+public:
+    int maxPoints(vector<vector<int>>& points) {
+        int n = points.size(), ans = 1;
+        for (int i=0; i<n; i++) {
+            unordered_map<double, int> ump;
+            for (int j=i+1; j<n; j++) {
+                double m;
+                int p = (points[j][1] - points[i][1]);
+                int q = (points[j][0] - points[i][0]);
+                if (q == 0) m = INT_MAX;
+                else m = (double)p/(double)q;
+                cout<<p<<" "<<q<<" "<<m<<endl;
+                ump[m]++;
+            }
+            // for (auto i: ump) cout<<i.first<<" "<<i.second<<" , ";
+            cout<<endl;
+            for (auto i: ump) ans = max(ans, i.second+1);
+        }
+        return ans;
+    }
+};
