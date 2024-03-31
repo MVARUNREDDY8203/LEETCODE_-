a5592fecdda74cf66e7e8c5034eddaa9c38a4313
M VARUN  REDDY <94187286+MVARUNREDDY8203@users.noreply.github.com>
2024-03-31 19:23:36 +0530

    Time: 0 ms (100.00%) | Memory: 8.7 MB (90.65%) - LeetSync

--- 140-word-break-ii/word-break-ii.cpp
+++ 140-word-break-ii/word-break-ii.cpp
@@ -2,20 +2,17 @@ class Solution {
 public:
     vector<string> ans;
     unordered_set<string> uset;
-    bool util (int i, string curr, string &s) {
+    void util (int i, string curr, string &s) {
         if (i >= s.size()) {
             ans.push_back(curr);
-            return true;
+            return;
         }
         if (curr.size() > 0) curr += " ";
 
-        bool possible = false;
         for (int j=i; j<s.size(); j++) {
             string p = s.substr(i, j-i+1);
-            if (uset.count(p) && util(j+1, curr+p, s)) possible = true;
+            if (uset.count(p)) util(j+1, curr+p, s);
         }
-
-        return possible;
     }
     vector<string> wordBreak(string s, vector<string>& wordDict) {
         for (auto i: wordDict) uset.insert(i);
