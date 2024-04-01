1899cac97ecd0ddf574b260a33a3a2ee94aaa95c
M VARUN  REDDY <94187286+MVARUNREDDY8203@users.noreply.github.com>
2024-04-01 21:05:18 +0530

    Time: 7 ms (95.90%) | Memory: 25.9 MB (99.22%) - LeetSync

---
+++ 695-max-area-of-island/max-area-of-island.cpp
@@ -0,0 +1,29 @@
+class Solution {
+private:
+    int m;
+    int n;
+public:
+    int util(int i, int j, vector<vector<int>>&grid) {
+        if (i < 0 || j < 0 || i >= m || j >= n) return 0;
+        int ans = 0;
+        if (grid[i][j] == 1) {
+            grid[i][j] = 2;
+            ans += util(i+1, j, grid);
+            ans += util(i-1, j, grid);
+            ans += util(i, j+1, grid);
+            ans += util(i, j-1, grid);
+            return ans + 1;
+        }
+        return 0;
+    }
+    int maxAreaOfIsland(vector<vector<int>>& grid) {
+        m = grid.size(), n = grid[0].size();
+        int max_area = 0;
+        for (int i=0; i<m; i++) {
+            for (int j=0; j<n; j++) {
+                if (grid[i][j] == 1) max_area = max(max_area, util(i, j, grid));
+            }
+        }
+        return max_area;
+    }
+};
