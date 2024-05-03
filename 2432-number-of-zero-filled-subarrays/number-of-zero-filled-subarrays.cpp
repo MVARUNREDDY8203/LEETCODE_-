445c7736b885d3f0f2b4f7bfdeef0ece137d0710
M VARUN  REDDY <94187286+MVARUNREDDY8203@users.noreply.github.com>
2024-05-03 19:56:01 +0530

    Time: 57 ms (99.10%) | Memory: 108.8 MB (96.29%) - LeetSync

--- 2432-number-of-zero-filled-subarrays/number-of-zero-filled-subarrays.cpp
+++ 2432-number-of-zero-filled-subarrays/number-of-zero-filled-subarrays.cpp
@@ -1,6 +1,7 @@
 class Solution {
 public:
     long long zeroFilledSubarray(vector<int>& nums) {
+        ios_base::sync_with_stdio(false);cin.tie(0);cout.tie(0);
         long long int n = nums.size(), prev_cnt = 0, total_cnt = 0;
         for (int i=0; i<n; i++) {
             if (nums[i] == 0) {
