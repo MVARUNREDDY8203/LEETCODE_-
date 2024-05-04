3cb63e975d17399ad5051e1c2e67cebc7f9e67d5
M VARUN  REDDY <94187286+MVARUNREDDY8203@users.noreply.github.com>
2024-05-04 09:51:38 +0530

    Time: 16 ms (94.03%) | Memory: 11.6 MB (63.65%) - LeetSync

--- 2487-optimal-partition-of-string/optimal-partition-of-string.cpp
+++ 2487-optimal-partition-of-string/optimal-partition-of-string.cpp
@@ -1,6 +1,7 @@
 class Solution {
 public:
     int partitionString(string s) {
+        ios_base::sync_with_stdio(false);cin.tie(0);cout.tie(0);
         int n = s.size(), cnt = 1;
         int freq[26];
         memset(freq, 0, sizeof(freq));
