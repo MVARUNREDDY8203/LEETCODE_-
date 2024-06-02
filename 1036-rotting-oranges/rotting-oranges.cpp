cc02738c39a43dcebbd3429e9f79feed3560ed28
M VARUN  REDDY <94187286+MVARUNREDDY8203@users.noreply.github.com>
2024-06-02 12:57:34 +0530

    Time: 19 ms (6.40%) | Memory: 21.4 MB (5.12%) - LeetSync

--- 1036-rotting-oranges/rotting-oranges.cpp
+++ 1036-rotting-oranges/rotting-oranges.cpp
@@ -4,10 +4,9 @@ public:
     vector<vector<int>> visited;
     vector<vector<int>> dist_map;
 
-    void bfs(int p, int q, int a, vector<vector<int>> &grid) {
-        int max_dist = 0;
+    void bfs(int p, int q, int a, vector<vector<int>> &grid) {  // p, q is the infection starter node, a is the u_id of the nth rotten apple, used to account for the nth bfs/visited map
         int m = grid.size(), n = grid[0].size();
-        que.push({p-1, q, 1});
+        que.push({p-1, q, 1}); // infection spreads 4d way from the root/rotten apple
         que.push({p+1, q, 1});
         que.push({p, q-1, 1});
         que.push({p, q+1, 1});
@@ -18,44 +17,41 @@ public:
             int dist = que.front()[2];
             que.pop();
             
-            if (i < 0 || i >= m || j < 0 || j >= n || grid[i][j] != 1 || visited[i][j] == a) continue;
+            if (i < 0 || i >= m || j < 0 || j >= n || grid[i][j] != 1 || visited[i][j] == a) continue; // infection cannot spread out_of_bounds|to an apple that is not fresh|if it has already been visited by the ath infection
 
-            max_dist = max(max_dist, dist);
-            dist_map[i][j] = min(dist_map[i][j], dist);
+            dist_map[i][j] = min(dist_map[i][j], dist);     // taking min dist/time of infection spread 
             visited[i][j] = a;
 
-            que.push({i, j+1, dist+1});
+            que.push({i, j+1, dist+1});     // infection spread;
             que.push({i, j-1, dist+1});
             que.push({i+1, j, dist+1});
             que.push({i-1, j, dist+1});
         }
     }
     int orangesRotting(vector<vector<int>>& grid) {
+        ios_base::sync_with_stdio(false); cin.tie(0); cout.tie(0);
         int m = grid.size(), n = grid[0].size();
-        dist_map.resize(m, vector<int>(n, INT_MAX));
-        visited.resize(m, vector<int>(n, -1));
+        dist_map.resize(m, vector<int>(n, INT_MAX)); // to account the distances/time of the infection spread
+        visited.resize(m, vector<int>(n, -1));      // to account if the apple is visited or not globally accessible
 
         int a = 1;
         for (int i=0; i<m; i++) {
             for (int j=0; j<n; j++) {
                 if (grid[i][j] == 2) {
-                    bfs(i, j, a++, grid);
+                    bfs(i, j, a++, grid);  // if the apple is rotten, we call bfs to spread infection
                 }
             }
         }
 
-        int ans = -1;
+        int ans = -1; // default -1 value initialized
         for (int i=0; i<m; i++) {
             for (int j=0; j<n; j++) {
-                cout<<dist_map[i][j]<<" ";
-
-                if (grid[i][j] == 1) {
-                    if (visited[i][j] == -1) return -1;
-                    ans = max(ans, dist_map[i][j]);
+                if (grid[i][j] == 1) { 
+                    if (visited[i][j] == -1) return -1; // if the apple is fresh and is never visited return -1
+                    ans = max(ans, dist_map[i][j]); // if the apple was originally fresh, we take the maximum of the dist_map[i][j] as the ans as this is the last apple to get infection
                 }
             }
-            cout<<endl;
         }
-        return ans == -1 ? 0 : ans;
+        return ans == -1 ? 0 : ans; // if ans is not updated from -1, this means there are no 1s in the array, meaning no infection would spread and time = 0
     }
 };
