16862783bffed596a9016c0f2c871fed595c506f
M VARUN  REDDY <94187286+MVARUNREDDY8203@users.noreply.github.com>
2024-06-03 22:23:23 +0530

    Time: 555 ms (17.96%), Space: 184.7 MB (6.87%) - LeetHub

---
+++ 1576-reorder-routes-to-make-all-paths-lead-to-the-city-zero/1576-reorder-routes-to-make-all-paths-lead-to-the-city-zero.cpp
@@ -0,0 +1,30 @@
+class Solution {
+public:
+    
+    int minReorder(int n, vector<vector<int>>& connections) {
+        vector<vector<vector<int>>> adj(n);
+        for (auto i: connections) {
+            adj[i[0]].push_back({i[1], 1});
+            adj[i[1]].push_back({i[0], 0});
+        }
+
+        stack<int> stk;
+        int cnt = 0;
+        stk.push(0);
+        set<int> visited;
+        while (!stk.empty()) {
+            int parent = stk.top();
+            stk.pop();
+            for (auto i: adj[parent]) {
+                if (!visited.count(i[0])) {
+                    int child = i[0];
+                    int sign = i[1];
+                    cnt += sign;
+                    stk.push(child);
+                }
+            }
+            visited.insert(parent);
+        }
+        return cnt;
+    }
+};
