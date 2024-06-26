c947859676cd97edb73d404a520d9ed4961092f3
M VARUN  REDDY <94187286+MVARUNREDDY8203@users.noreply.github.com>
2024-06-26 15:42:05 +0530

    Time: 8 ms (58.15%) | Memory: 8.5 MB (57.62%) - LeetSync

--- 567-permutation-in-string/permutation-in-string.cpp
+++ 567-permutation-in-string/permutation-in-string.cpp
@@ -1,90 +1,20 @@
 class Solution {
 public:
     bool checkInclusion(string s1, string s2) {
-        if (s1.size() > s2.size()) return false;
-
-        unordered_map<int, int> curr_count;
-        for (auto i: s1) curr_count[i]++;
-
-        // sliding window starter
-        for (int i=0; i < s1.size(); i++) {
-            curr_count[s2[i]]--;
-            if (curr_count[s2[i]] == 0) curr_count.erase(s2[i]);
-        }
-        if (curr_count.size() == 0) return true;
-
-        // sliding window propogation
-        for (int j = s1.size(); j<s2.size(); j++) {
-            curr_count[s2[j-s1.size()]]++;
-            if (curr_count[s2[j-s1.size()]] == 0) curr_count.erase(s2[j-s1.size()]);
-
-            curr_count[s2[j]]--;
-            // if (curr_count.count(s2[j])) curr_count[s2[j]]--;
-
-            if (curr_count[s2[j]] == 0) curr_count.erase(s2[j]);
-            if (curr_count.size() == 0) return true;
-            cout<<j<<" "<<curr_count.size()<<endl;
+        int n = s2.size(), m = s1.size();
+        vector<int> freq_map(26, 0);
+        int l=-1, r = 0, cnt = 0;
+        for (int i=0; i<m; i++) freq_map[s1[i] - 'a']++;
+        while (r<n) {
+            freq_map[s2[r] - 'a']--;
+            cnt++;
+            while (freq_map[s2[r] - 'a'] < 0) {
+                freq_map[s2[++l] - 'a']++;
+                cnt--;
+            }
+            if (cnt == m) {cout<<l<<" "<<r<<endl;return true; }  
+            r++;     
         }
         return false;
     }
 };
-
-
-// class Solution {
-// public:
-//     bool checkInclusion(string s1, string s2) {
-//         if (s1.size() > s2.size()) return false;
-
-//         unordered_map<int, int> count1;
-//         for (auto i: s1) count1[i]++;
-
-//         unordered_map<int, int> curr_count = count1;
-//         // sliding window starter
-//         for (int i=0; i < s1.size(); i++) {
-//             curr_count[s2[i]]--;
-//             if (curr_count[s2[i]] == 0) curr_count.erase(s2[i]);
-//         }
-//         if (curr_count.size() == 0) return true;
-
-//         // sliding window propogation
-//         for (int j = s1.size(); j<s2.size(); j++) {
-//             curr_count[s2[j-s1.size()]]++;
-//             if (curr_count[s2[j-s1.size()]] == 0) curr_count.erase(s2[j-s1.size()]);
-
-//             curr_count[s2[j]]--;
-//             // if (curr_count.count(s2[j])) curr_count[s2[j]]--;
-
-//             if (curr_count[s2[j]] == 0) curr_count.erase(s2[j]);
-//             if (curr_count.size() == 0) return true;
-//             cout<<j<<" "<<curr_count.size()<<endl;
-//         }
-//         return false;
-//     }
-// };
-
-
-// class Solution {
-// public:
-//     bool checkInclusion(string s1, string s2) {
-//         if (s1.size() > s2.size()) return false;
-
-//         unordered_map<int, int> count1;
-//         for (auto i: s1) count1[i]++;
-//         for (int start=0; start<s2.size(); start++) {
-//             if (count1.count(s2[start])) {
-//                 unordered_map<int, int> curr_count = count1;
-//                 for (int end = start; end<s2.size(); end++) {
-//                     if (curr_count.count(s2[end])) curr_count[s2[end]]--;
-//                     else {
-//                         break;
-//                     }
-
-//                     if (curr_count[s2[end]] == 0) curr_count.erase(s2[end]);
-//                     if (curr_count.size() == 0) return true;
-//                 }
-//             }
-//         }
-
-//         return false;
-//     }
-// };
