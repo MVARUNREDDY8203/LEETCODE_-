136921df1a45572de004ecc411b20973e16d5802
M VARUN  REDDY <94187286+MVARUNREDDY8203@users.noreply.github.com>
2024-06-26 23:55:40 +0530

    Time: 33 ms (99.63%) | Memory: 62 MB (45.26%) - LeetSync

--- 882-peak-index-in-a-mountain-array/peak-index-in-a-mountain-array.cpp
+++ 882-peak-index-in-a-mountain-array/peak-index-in-a-mountain-array.cpp
@@ -1,6 +1,7 @@
 class Solution {
 public:
     int peakIndexInMountainArray(vector<int>& nums) {
+        ios_base::sync_with_stdio(0);cin.tie(0);cout.tie(0);
         int l = 0, r = nums.size()-1, mid = (l+r)/2;
 
         while (l < r-1) {
