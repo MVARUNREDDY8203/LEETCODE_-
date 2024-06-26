1b6bea20b8ac06cf6a654b7b5a203d273ddfc2a7
M VARUN  REDDY <94187286+MVARUNREDDY8203@users.noreply.github.com>
2024-06-26 21:34:58 +0530

    Time: 208 ms (20.14%) | Memory: 97.5 MB (13.76%) - LeetSync

---
+++ 883-car-fleet/car-fleet.cpp
@@ -0,0 +1,26 @@
+class Solution {
+public:
+    int carFleet(int target, vector<int>& pos, vector<int>& speed) {
+        int n = pos.size();
+        vector<vector<double>> arr(n);
+        for (int i=0; i<n; i++) {
+            double time = (double)(target-pos[i])/(double)speed[i];
+            arr[i] = {(double)pos[i], time};
+        }
+        sort(arr.begin(), arr.end(), [](const vector<double> &a, const vector<double>&b) {
+            return a[0] < b[0];
+        });
+        // for(auto i: arr) {
+        //     cout<<i[0]<<" "<<i[1]<<endl;
+        // }
+
+        stack<double> stk;
+        stk.push(arr[n-1][1]);
+        int i=n-2;
+        while (i >= 0) {
+            while (i>=0 && stk.top() >= (double)arr[i][1]) i--;
+            if (i >=0) stk.push(arr[i][1]);
+        }
+        return stk.size();
+    }
+};
