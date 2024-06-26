7429ac652b470137fce4817b0241704c6e79dcd4
M VARUN  REDDY <94187286+MVARUNREDDY8203@users.noreply.github.com>
2024-06-26 15:16:20 +0530

    Time: 53 ms (98.89%) | Memory: 100 MB (5.01%) - LeetSync

--- 121-best-time-to-buy-and-sell-stock/best-time-to-buy-and-sell-stock.cpp
+++ 121-best-time-to-buy-and-sell-stock/best-time-to-buy-and-sell-stock.cpp
@@ -1,6 +1,7 @@
 class Solution {
 public:
     int maxProfit(vector<int>& prices) {
+        ios_base::sync_with_stdio(false); cin.tie(nullptr); cout.tie(nullptr);
         int n = prices.size();
         vector<int> next_max(n, prices[n-1]);
         for (int i=n-2; i>=0; i--) next_max[i] = max(next_max[i+1], prices[i]);
