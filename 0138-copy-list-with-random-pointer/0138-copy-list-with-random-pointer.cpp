034af3de07c927a6fa3c26f700c37bc6bb443d90
M VARUN  REDDY <94187286+MVARUNREDDY8203@users.noreply.github.com>
2024-06-30 23:13:04 +0530

    Time: 3 ms (91.24%), Space: 13.6 MB (10.92%) - LeetHub

--- 0138-copy-list-with-random-pointer/0138-copy-list-with-random-pointer.cpp
+++ 0138-copy-list-with-random-pointer/0138-copy-list-with-random-pointer.cpp
@@ -1,69 +1,36 @@
+/*
+// Definition for a Node.
+class Node {
+public:
+    int val;
+    Node* next;
+    Node* random;
+    
+    Node(int _val) {
+        val = _val;
+        next = NULL;
+        random = NULL;
+    }
+};
+*/
+
 class Solution {
 public:
     Node* copyRandomList(Node* head) {
-        unordered_map<Node*, Node*> mp;
+        unordered_map<Node*, Node*> ump;
+        Node* starter = new Node(0);
+
         Node* curr = head;
-        while(curr != NULL){
-            mp[curr] = new Node(curr->val);
+        while (curr) {
+            ump[curr] = new Node(curr->val);
             curr = curr->next;
         }
-
         curr = head;
-        while(curr != NULL){
-            mp[curr]->next = mp[curr->next];
-            mp[curr]->random = mp[curr->random];
+        while (curr) {
+            ump[curr]->next = ump[curr->next];
+            ump[curr]->random = ump[curr->random];
             curr = curr->next;
         }
-
-        curr = head;
-        return mp[curr];
+        return ump[head];
     }
 };
-
-
-// class Solution {
-// public:
-//     int depth_finder(Node* curr, Node* target){
-//         if(target == NULL) return 2000;
-//         if(curr == target) return 0;
-//         return 1 + depth_finder(curr->next, target);
-//     }
-//     Node* copyRandomList(Node* head) {
-//         if(head == NULL) return head;
-
-//         //creating a ll copy with head 2
-//         Node* curr = head;
-//         Node* head2 = new Node(head->val);
-//         Node* prev = head2;
-//         curr = curr->next;
-//         vector<pair<Node*, int>> dir;
-//         dir.push_back({head2,-1});
-//         int n = 1;
-
-//         while(curr != NULL){
-//             Node* temp = new Node(curr->val);
-//             prev->next  = temp;
-//             dir.push_back({temp, -1});
-//             prev = temp;
-//             curr = curr->next;
-//             n++;
-//         }
-
-//         //finding random ptrses pointing addrs
-//         curr = head;
-//         int index = 0;
-//         while(curr != NULL){
-//             dir[index].second = depth_finder(head, curr->random);
-//             curr = curr->next;
-//             index++;
-//         }
-        
-//         Node* temp3 = head2;
-//         for(int i=0; i<dir.size(); i++){
-//             if(dir[i].second != 2000) temp3->random = dir[dir[i].second].first;
-//             else temp3->random = NULL;
-//             temp3 = temp3->next;
-//         }
-//         return head2;
-//     }
-// };
