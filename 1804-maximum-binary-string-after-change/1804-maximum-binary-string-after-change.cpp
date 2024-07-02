004cd7b0ff7b57387086b9765731fd78773b5c5f
M VARUN  REDDY <94187286+MVARUNREDDY8203@users.noreply.github.com>
2024-07-02 17:11:54 +0530

    Time: 127 ms (78.62%), Space: 54.4 MB (11.72%) - LeetHub

--- 1804-maximum-binary-string-after-change/1804-maximum-binary-string-after-change.cpp
+++ 1804-maximum-binary-string-after-change/1804-maximum-binary-string-after-change.cpp
@@ -1,8 +1,8 @@
 class Solution {
 public:
     string maximumBinaryString(string binary) {
+        ios_base::sync_with_stdio(false);cin.tie(nullptr);cout.tie(nullptr);
         int zero_cnt = 0, n = binary.size();
-        // for (auto i: binary) if (i == '0') zero_cnt++;
 
         int first_zero = -1, ones_cnt_after_fz = 0, ones_cnt_bef_fz = 0;
         for (int i=0; i<n; i++) {
@@ -17,9 +17,8 @@ public:
         }
         
         if (zero_cnt <= 1) return binary;
-        
+
         string ans = "";
-        cout<<zero_cnt<<" "<<ones_cnt_bef_fz<<" "<<ones_cnt_after_fz<<endl;
         for (int i=0; i<ones_cnt_bef_fz; i++) ans += "1";
         for (int i=0; i<zero_cnt-1; i++) ans += "1";
         ans += "0";
