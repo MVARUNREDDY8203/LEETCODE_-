a0707963d5ff55484868a03ffcb25e7642029c5c
M VARUN  REDDY <94187286+MVARUNREDDY8203@users.noreply.github.com>
2024-07-06 00:31:05 +0530

    Time: 39 ms (62.66%), Space: 33.5 MB (70.58%) - LeetHub

--- 1056-capacity-to-ship-packages-within-d-days/1056-capacity-to-ship-packages-within-d-days.cpp
+++ 1056-capacity-to-ship-packages-within-d-days/1056-capacity-to-ship-packages-within-d-days.cpp
@@ -1,34 +1,25 @@
 class Solution {
 public:
-    int days_calculator(vector<int>&weights, int max_load) {
-        int days = 1, curr_load = 0;
-        for (int i=0; i<weights.size(); i++) {
-            curr_load += weights[i];
-            if (curr_load > max_load) {
-                days++;
-                curr_load = weights[i];
-            }
-        }
-        return days;
-    }
     int shipWithinDays(vector<int>& weights, int days) {
-        int max_weight = -1, total = 0;
-        for (auto i: weights) {
-            total += i;
-            max_weight = max(i, max_weight);
-        }
-
-        int l = max_weight, r = total, mid = 0;
+        int sum = 0, max_weight = 0, n = weights.size();
+        for (auto i: weights) sum += i, max_weight = max(i, max_weight);
+        
+        int r = sum, l = max_weight;
         while (l < r) {
-            mid = (l + r) / 2;
-            int curr_days = days_calculator(weights, mid);
+            int mid_capacity = l + (r-l)/2;
+            int curr_days = 1, tank = 0;
+            for (int i=0; i<n; i++) {
+                tank += weights[i];
+                if (tank > mid_capacity) {
+                    tank = weights[i];
+                    curr_days++;
+                }
+            }
+
             if (curr_days > days) {
-                l = mid + 1;
+                l = mid_capacity+1;
             }
-            else {
-                r = mid;
-            }
-            
+            else r = mid_capacity;
         }
         return l;
     }
