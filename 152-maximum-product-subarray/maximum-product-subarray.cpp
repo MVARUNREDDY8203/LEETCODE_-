72d44e4b01a2a8c58fe393111ef68cd30c1c73a7
M VARUN  REDDY <94187286+MVARUNREDDY8203@users.noreply.github.com>
2024-07-07 14:45:27 +0530

    Time: 5 ms (59.68%) | Memory: 16.2 MB (84.26%) - LeetSync

--- 152-maximum-product-subarray/maximum-product-subarray.cpp
+++ 152-maximum-product-subarray/maximum-product-subarray.cpp
@@ -1,16 +1,15 @@
 class Solution {
 public:
     int maxProduct(vector<int>& nums) {
-        long double max_prod = 1, min_prod = 1, n = nums.size(), ans = nums[0];
-        for (auto i: nums) ans = max(ans, (long double)i);
-        for (auto curr: nums) {
-            long double max_prod_copy = max_prod; 
-            max_prod = max((long double)curr, max(max_prod*curr, min_prod*curr));
-            min_prod = min((long double)curr, min(max_prod_copy*curr, min_prod*curr));
-            cout<<curr<<" "<<max_prod<<" "<<min_prod<<endl;
-            ans = max(ans, max_prod);
+        long double prefix_prod = 1, suffix_prod = 1, n = nums.size(), ans = INT_MIN;
+        for (int i=0; i<n; i++) {
+            if (suffix_prod == 0) suffix_prod = 1;
+            if (prefix_prod == 0) prefix_prod = 1;
+
+            prefix_prod *= nums[i];
+            suffix_prod *= nums[n-1 - i];
+            ans = max({ans, prefix_prod, suffix_prod});
         }
-        cout<<ans;
         return (int)ans;
     }
 };
