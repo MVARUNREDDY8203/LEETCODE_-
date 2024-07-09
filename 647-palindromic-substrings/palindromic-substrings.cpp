7c79b919fa1bb5e31a0405ee80efe42350a2c852
M VARUN  REDDY <94187286+MVARUNREDDY8203@users.noreply.github.com>
2024-07-09 23:36:41 +0530

    Time: 28 ms (35.35%) | Memory: 9.9 MB (64.64%) - LeetSync

--- 647-palindromic-substrings/palindromic-substrings.cpp
+++ 647-palindromic-substrings/palindromic-substrings.cpp
@@ -1,24 +1,25 @@
 class Solution {
 public:
     int countSubstrings(string s) {
-        int cnt = 0, n = s.size();
-        for (int i=0; i<n; i++) {
-            cnt++;
-            int l = i-1, r = i+1;
-            while (l >= 0 && r <= n-1) {
-                if (s[l] != s[r]) break;
+        int n = s.size();
+        vector<vector<bool>> isPalindrome(n, vector<bool>(n, false));
+
+        int cnt = 0;
+        for (int i=0; i<n; i++) isPalindrome[i][i] = true, cnt++;
+
+        for (int i=0; i<n-1; i++) {
+            if (s[i] == s[i+1]) {
+                isPalindrome[i][i+1] = true;
                 cnt++;
-                l--;
-                r++;
             }
         }
-        for (int i=0; i<n-1; i++) {
-            int l = i, r = i+1;
-            while (l >= 0 && r <= n-1) {
-                if (s[l] != s[r]) break;
-                cnt++;
-                l--;
-                r++;
+
+        for (int len=3; len<=n; len++) {
+            for (int i=0; i<n-len+1; i++) {
+                if (s[i] == s[i+len-1] && isPalindrome[i+1][i+len-2]) {
+                    isPalindrome[i][i+len-1] = true;
+                    cnt++;
+                }
             }
         }
         return cnt;
