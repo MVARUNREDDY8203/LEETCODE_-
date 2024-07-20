8cc6ee527fb43b3b4da85cccc3fb67b3e81c80b4
M VARUN  REDDY <94187286+MVARUNREDDY8203@users.noreply.github.com>
2024-07-20 17:46:16 +0530

    Time: 108 ms (11.19%) | Memory: 25.6 MB (10.05%) - LeetSync

--- 221-maximal-square/maximal-square.cpp
+++ 221-maximal-square/maximal-square.cpp
@@ -2,6 +2,8 @@ class Solution {
 public:
     int maximalSquare(vector<vector<char>>& matrix) {
 
+        ios_base::sync_with_stdio(false);cin.tie(nullptr);cout.tie(nullptr);
+
         int m = matrix.size(), n = matrix[0].size();
         vector<int> histogram(n, 0);
         int ans = 0;
@@ -11,8 +13,8 @@ public:
                 if (matrix[i][j] - '0') histogram[j]++;
                 else histogram[j] = 0;
             }
-            for (auto m: histogram) cout<<m<<" ";
-            cout<<endl;
+            // for (auto m: histogram) cout<<m<<" ";
+            // cout<<endl;
             histogram.push_back(0);
 
             stack<int> stk;
@@ -28,15 +30,6 @@ public:
                 }
                 stk.push(j);
             }
-            // while (!stk.empty()) {
-            //     int right_idx = j+1;
-            //     int curr_idx = stk.top();
-            //     stk.pop();
-            //     int left_idx = stk.empty() ? -1 : stk.top();
-            //     int curr_area = (right_idx - left_idx -1) >= histogram[curr_idx] ? histogram[curr_idx] * histogram[curr_idx] : 0;
-            //     cout<<curr_area<<endl;
-            //     ans = max(ans, curr_area);
-            // }
         }
         return ans;
     }
