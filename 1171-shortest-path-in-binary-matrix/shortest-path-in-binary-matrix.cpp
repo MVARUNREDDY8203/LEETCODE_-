2fc77fa74d039ef6a5eb872f00e14302a2d3e5ab
M VARUN  REDDY <94187286+MVARUNREDDY8203@users.noreply.github.com>
2024-07-23 15:10:05 +0530

    Time: 78 ms (21.58%) | Memory: 43.8 MB (9.83%) - LeetSync

--- 1171-shortest-path-in-binary-matrix/shortest-path-in-binary-matrix.cpp
+++ 1171-shortest-path-in-binary-matrix/shortest-path-in-binary-matrix.cpp
@@ -3,13 +3,13 @@ public:
     int shortestPathBinaryMatrix(vector<vector<int>>& grid) {
         ios_base::sync_with_stdio(false);cin.tie(nullptr);cout.tie(nullptr);
         int m = grid.size(), n = grid[0].size();
-        queue<vector<int>> que;
-        que.push({0, 0, 1});
+        queue<pair<pair<int, int>, int>> que;
+        que.push({{0, 0}, 1});
 
         while (!que.empty()) {
-            int i = que.front()[0];
-            int j = que.front()[1];
-            int dist = que.front()[2];
+            int i = que.front().first.first;
+            int j = que.front().first.second;
+            int dist = que.front().second;
             que.pop();
 
             if (i < 0 || i >= m || j < 0 || j >= n) continue;
@@ -18,14 +18,14 @@ public:
                 if (i == m-1 && j == n-1) return dist;
                 grid[i][j] = -1;
 
-                que.push({i-1, j, dist+1});
-                que.push({i-1, j+1, dist+1});
-                que.push({i-1, j-1, dist+1});
-                que.push({i, j+1, dist+1});
-                que.push({i, j-1, dist+1});
-                que.push({i+1, j, dist+1});
-                que.push({i+1, j+1, dist+1});
-                que.push({i+1, j-1, dist+1});
+                que.push({{i-1, j}, dist+1});
+                que.push({{i-1, j+1}, dist+1});
+                que.push({{i-1, j-1}, dist+1});
+                que.push({{i, j+1}, dist+1});
+                que.push({{i, j-1}, dist+1});
+                que.push({{i+1, j}, dist+1});
+                que.push({{i+1, j+1}, dist+1});
+                que.push({{i+1, j-1}, dist+1});
             }
         }
         return -1;
