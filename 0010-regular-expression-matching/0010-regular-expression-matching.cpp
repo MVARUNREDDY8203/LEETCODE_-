fcf75c08cd4f8354f4e651588ab912904f8e6184
M VARUN  REDDY <94187286+MVARUNREDDY8203@users.noreply.github.com>
2024-07-26 11:46:53 +0530

    Time: 4 ms (54.46%), Space: 8.9 MB (40.04%) - LeetHub

--- 0010-regular-expression-matching/0010-regular-expression-matching.cpp
+++ 0010-regular-expression-matching/0010-regular-expression-matching.cpp
@@ -1,5 +1,6 @@
 class Solution {
 public:
+    int dp[21][21];
     bool util(int i, int j, string &s, string &p) {
         if (j >= p.size()) {
             if (i >= s.size()) return true;
@@ -9,20 +10,22 @@ public:
             if (p[j+1] == '*') return util(i, j+2, s, p);
             return false;
         }
+        if (dp[i][j] != -1) return dp[i][j];
 
         if (j+1 < p.size() && p[j+1] == '*') {
             bool ans = false;
             ans |= util(i, j+2, s, p);
             if (p[j] == '.' || p[j] == s[i]) ans |= util(i+1, j, s, p);
-            return ans;
+            return dp[i][j] = ans;
         }
 
-        if (p[j] == '.') return util(i+1, j+1, s, p);
+        if (p[j] == '.') return dp[i][j] = util(i+1, j+1, s, p);
 
-        if (p[j] == s[i]) return util(i+1, j+1, s, p);
-        return false;
+        if (p[j] == s[i]) return dp[i][j] = util(i+1, j+1, s, p);
+        return dp[i][j] = false;
     }
     bool isMatch(string s, string p) {
+        memset(dp, -1, sizeof(dp));
         return util(0, 0, s, p);
     }
 };
