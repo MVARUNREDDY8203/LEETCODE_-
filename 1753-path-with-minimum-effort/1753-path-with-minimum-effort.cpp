ad2390ba0dc8f3b3edca447618b463a3f96ade82
M VARUN  REDDY <94187286+MVARUNREDDY8203@users.noreply.github.com>
2024-07-30 14:28:45 +0530

    Time: 348 ms (5.15%), Space: 70 MB (5.81%) - LeetHub

--- 1753-path-with-minimum-effort/1753-path-with-minimum-effort.cpp
+++ 1753-path-with-minimum-effort/1753-path-with-minimum-effort.cpp
@@ -1,24 +1,18 @@
 class Solution {
 public:
-    struct cmp {
-        bool operator() (vector<int> const &a, vector<int> const &b) {
-            return a[0] < b[0];
-        }
-    };
+    
     int minimumEffortPath(vector<vector<int>>& heights) {
+        ios_base::sync_with_stdio(false);cin.tie(nullptr);cout.tie(nullptr);
         int m = heights.size(), n = heights[0].size();
 
         priority_queue<vector<int>, vector<vector<int>>, greater<vector<int>>> pq;
-        // if (1 < n) pq.push({abs(heights[0][0] - heights[0][1]), 0, 1});
-        // if (1 < m) pq.push({abs(heights[0][0] - heights[1][0]), 1, 0});
-        // heights[0][0] = -1;
         pq.push({0, 0, 0});
 
         while (!pq.empty()) {
             int diff = pq.top()[0];
             int i = pq.top()[1];
             int j = pq.top()[2];
-            cout<<i<<" "<<j<<" "<<diff<<endl;
+            // cout<<i<<" "<<j<<" "<<diff<<endl;
             pq.pop();
 
             if (i < 0 || i >= m || j < 0 || j >= n) continue;
