07b828eb50fe6dbb9674fd02072fa5119671ccb1
M VARUN  REDDY <94187286+MVARUNREDDY8203@users.noreply.github.com>
2024-07-30 22:26:46 +0530

    Time: 173 ms (5.04%) | Memory: 49.5 MB (10.31%) - LeetSync

--- 744-network-delay-time/network-delay-time.cpp
+++ 744-network-delay-time/network-delay-time.cpp
@@ -2,20 +2,19 @@ class Solution {
 public:
     int networkDelayTime(vector<vector<int>>& times, int n, int k) {
         vector<int> sig_reached(n+1, 0);
-        map<pair<int, int>, int> edge;
+        // map<pair<int, int>, int> edge;
+        vector<vector<pair<int, int>>> adj(n+1);
         for (auto vec: times) {
-            int src = vec[0], dest = vec[1], time = vec[2];
-            edge[{src, dest}] = time;
+            adj[vec[0]].push_back({vec[1], vec[2]});
         }
 
-        priority_queue<vector<int>, vector<vector<int>>, greater<vector<int>>> pq;
-        pq.push({0, 0, k});
+        priority_queue<pair<int, int>, vector<pair<int, int>>, greater<pair<int, int>>> pq;
+        pq.push({0, k});
         int ans = 0;
         while (!pq.empty()) {
-            int time = pq.top()[0];
-            int prev = pq.top()[1];
-            int curr = pq.top()[2];
-            cout<<prev<<" "<<curr<<" "<<time<<endl;
+            int time = pq.top().first;
+            int curr = pq.top().second;
+            cout<<" "<<curr<<" "<<time<<endl;
             pq.pop();
 
             if (sig_reached[curr]) continue;
@@ -23,10 +22,8 @@ public:
             sig_reached[curr]++;
             ans = max(ans, time);
 
-            for (int k=0; k<=n; k++) {
-                if (edge.count({curr, k})) {
-                    pq.push({time + edge[{curr, k}], curr, k});
-                }
+            for (auto neighbour: adj[curr]) {
+                pq.push({time + neighbour.second, neighbour.first});
             }
         }
         for (int i=1; i<=n; i++) if (!sig_reached[i]) return -1;
