f3bf9c18fbdf98875bbd9fb45810a3f9c436d068
M VARUN  REDDY <94187286+MVARUNREDDY8203@users.noreply.github.com>
2024-08-07 11:50:34 +0530

    Time: 552 ms (24.92%), Space: 106 MB (65.29%) - LeetHub

---
+++ 2438-find-closest-node-to-given-two-nodes/2438-find-closest-node-to-given-two-nodes.cpp
@@ -0,0 +1,35 @@
+class Solution {
+public:
+    vector<int> dfs1;
+    vector<int> dfs2;
+    void util(int i, int dist, vector<int> &dfs_i, vector<int>&edges) {
+        if (edges[i] == -2) return;
+        dfs_i[i] = dist;
+        if (edges[i] == -1) return;
+        
+        int temp = edges[i];
+        edges[i] = -2;
+        util(temp, dist+1, dfs_i, edges);
+        edges[i] = temp;
+    }
+    int closestMeetingNode(vector<int>& edges, int node1, int node2) {
+        int n = edges.size();
+        dfs1.resize(n+1, INT_MAX);
+        dfs2.resize(n+1, INT_MAX);
+
+        util(node1, 0, dfs1, edges);
+        util(node2, 0, dfs2, edges);
+
+        int ans_val = INT_MAX;
+        int ans_idx = -1;
+
+        for (int i =0; i<n; i++) cout<<dfs1[i]<<" "<<dfs2[i]<<endl;
+        for (int i=0; i<n; i++) {
+            if (max(dfs1[i], dfs2[i]) < ans_val) {
+                ans_val = max(dfs1[i], dfs2[i]);
+                ans_idx = i;
+            }
+        }
+        return ans_idx;
+    }
+};
