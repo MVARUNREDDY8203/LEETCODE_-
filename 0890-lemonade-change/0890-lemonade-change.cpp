13419323b21a6533a64826c3354df5e82d461583
M VARUN  REDDY <94187286+MVARUNREDDY8203@users.noreply.github.com>
2024-08-15 14:22:55 +0530

    Time: 78 ms (30.17%), Space: 85.9 MB (63.29%) - LeetHub

---
+++ 0890-lemonade-change/0890-lemonade-change.cpp
@@ -0,0 +1,39 @@
+class Solution {
+public:
+    bool lemonadeChange(vector<int>& bills) {
+        // int total = 0;
+        int change[] = {0, 0};
+        for (int i=0; i<bills.size(); i++) {
+            if (bills[i] == 5) {
+                change[0]++;
+                continue;
+            }
+            else if (bills[i] == 10) {
+                if (change[0] > 0)  { 
+                    change[0]--;
+                    change[1]++;
+                    continue;
+                }
+                return false;
+            }
+            else {
+                if (change[1] > 0) {
+                    if (change[0] > 0) {
+                        change[0]--;
+                        change[1]--;
+                        continue;
+                    }
+                    return false;
+
+                }
+                else if (change[0] >= 3) {
+                    change[0] -= 3;
+                    continue;
+                }
+                
+                return false;
+            }
+        }
+        return true;
+    }
+};
