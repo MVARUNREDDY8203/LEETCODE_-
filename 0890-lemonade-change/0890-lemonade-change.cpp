daf8fae8d0128521ec141504e2a07110564dbb62
M VARUN  REDDY <94187286+MVARUNREDDY8203@users.noreply.github.com>
2024-08-15 14:23:44 +0530

    Time: 53 ms (97.85%), Space: 86.1 MB (32.71%) - LeetHub

--- 0890-lemonade-change/0890-lemonade-change.cpp
+++ 0890-lemonade-change/0890-lemonade-change.cpp
@@ -1,7 +1,7 @@
 class Solution {
 public:
     bool lemonadeChange(vector<int>& bills) {
-        // int total = 0;
+        ios::sync_with_stdio(false); cin.tie(nullptr);
         int change[] = {0, 0};
         for (int i=0; i<bills.size(); i++) {
             if (bills[i] == 5) {
