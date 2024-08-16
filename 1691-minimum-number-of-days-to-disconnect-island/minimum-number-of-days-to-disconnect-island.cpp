8c1d8010fd47d4c86b402f09daca5e037388b8c2
M VARUN  REDDY <94187286+MVARUNREDDY8203@users.noreply.github.com>
2024-08-16 23:27:32 +0530

    Time: 56 ms (77.88%) | Memory: 12 MB (96.54%) - LeetSync

---
+++ 1691-minimum-number-of-days-to-disconnect-island/minimum-number-of-days-to-disconnect-island.cpp
@@ -0,0 +1,47 @@
+class Solution {
+public:
+    vector<vector<int>> visited;
+    void mark_island(int i, int j, vector<vector<int>> &grid) {
+        if (i < 0 || i >= visited.size() || j < 0 || j >= visited[0].size() || grid[i][j] == 0 || visited[i][j] == 1) return;
+        visited[i][j] = 1;
+        mark_island(i+1, j, grid);
+        mark_island(i-1, j, grid);
+        mark_island(i, j+1, grid);
+        mark_island(i, j-1, grid);
+    }
+    int count_islands(vector<vector<int>> &grid) {
+        int m = grid.size(), n = grid[0].size(), cnt = 0;
+        for (int i=0; i<m; i++) {
+            for (int j=0; j<n; j++) { 
+                if (grid[i][j] == 1 && visited[i][j] == 0) {
+                    mark_island(i, j, grid);
+                    cnt++;
+                }
+            }
+        }
+        for (int i=0; i<m; i++) for (int j=0; j<n; j++) visited[i][j] = 0;
+        return cnt;
+    }
+    int minDays(vector<vector<int>>& grid) {
+        int m = grid.size(), n = grid[0].size();
+        visited.resize(m, vector<int>(n, 0));
+        int island_cnt = count_islands(grid);
+        if (island_cnt == 0 || island_cnt > 1) return 0;
+        cout<<endl;
+
+        for (int i=0; i<m; i++) {
+            for (int j=0; j<n; j++) {
+                if (grid[i][j] == 1) {
+                    grid[i][j] = 0;
+                    int new_cnt = count_islands(grid);
+                    if (new_cnt > 1 || new_cnt < 1) {
+                        return 1;
+                    }
+                    grid[i][j] = 1;
+                }
+            }
+        }
+
+        return 2;
+    }
+};
