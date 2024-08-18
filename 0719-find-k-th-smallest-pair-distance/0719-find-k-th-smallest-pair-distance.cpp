b6fcbf2a8bcf51ca5dac9e828ce5a7baac8de510
M VARUN  REDDY <94187286+MVARUNREDDY8203@users.noreply.github.com>
2024-08-18 15:32:00 +0530

    Time: 6 ms (78.62%), Space: 14.4 MB (22.92%) - LeetHub

--- 0719-find-k-th-smallest-pair-distance/0719-find-k-th-smallest-pair-distance.cpp
+++ 0719-find-k-th-smallest-pair-distance/0719-find-k-th-smallest-pair-distance.cpp
@@ -1,22 +1,27 @@
 class Solution {
 public:
-    bool enough(vector<int>&nums, int k, int dist) {
-        int cnt = 0, i = 0, j = 0, n = nums.size();
-        while (i < n || j < n) {
-            while (j < n && nums[j] - nums[i] <= dist) j++;
-            cnt += (j-1)-i;
-            i++;
-        }
-        return cnt >= k;
-    }
+    
     int smallestDistancePair(vector<int>& nums, int k) {
+        function<int(vector<int>, int)> util = [&](vector<int> nums, int diff) -> int {
+            int n = nums.size();
+            int cnt = 0, slow = 0, fast = 0;
+            while (slow < n || fast < n) {
+                while (fast < n && nums[fast] - nums[slow] <= diff) {
+                    fast++;
+                }
+                cnt += fast-1 - slow;
+                slow++;
+            }
+            return cnt;
+        };
+        int n = nums.size();
         sort(nums.begin(), nums.end());
-        int n = nums.size(), l = 0, r = nums[n-1] - nums[0];
+        int r = nums[n-1] - nums[0];
+        int l = 0;
         while (l < r) {
             int mid = l + (r-l)/2;
-            if (enough(nums, k, mid)) {
-                r = mid;
-            }
+            int curr_cnt = util(nums, mid);
+            if (curr_cnt >= k) r = mid;
             else l = mid+1;
         }
         return l;
