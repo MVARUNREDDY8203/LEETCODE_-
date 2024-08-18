f036ccd2968137e49b3b7b92da7206af642df8d8
M VARUN  REDDY <94187286+MVARUNREDDY8203@users.noreply.github.com>
2024-08-18 21:43:10 +0530

    Time: 61 ms (93.22%), Space: 41 MB (73.9%) - LeetHub

---
+++ 2641-disconnect-path-in-a-binary-matrix-by-at-most-one-flip/2641-disconnect-path-in-a-binary-matrix-by-at-most-one-flip.cpp
@@ -0,0 +1,17 @@
+class Solution {
+public:
+    bool isPossibleToCutPath(vector<vector<int>>& grid) {
+        int m = grid.size(), n = grid[0].size();
+
+        function<bool(int, int)> dfs = [&](int i, int j) -> bool {
+            if (i >= m || j >= n || grid[i][j] == 0) return false;
+            if (i == m-1 && j == n-1) return true;
+
+            grid[i][j] = 0;
+            return dfs(i+1, j) || dfs(i, j+1);
+        };
+        dfs(0, 0);
+        grid[0][0] = 1;
+        return !dfs(0, 0);
+    }
+};
