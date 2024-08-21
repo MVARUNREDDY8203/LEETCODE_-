6ad0ef98921c86fd32716755c69c495bc4322e13
M VARUN  REDDY <94187286+MVARUNREDDY8203@users.noreply.github.com>
2024-08-21 11:59:16 +0530

    Time: 2 ms (71.01%) | Memory: 7.6 MB (42.24%) - LeetSync

--- 650-2-keys-keyboard/2-keys-keyboard.cpp
+++ 650-2-keys-keyboard/2-keys-keyboard.cpp
@@ -1,21 +1,10 @@
 class Solution {
 public:
-    int dp[1001];
-    bool isPrime(int a) {
-        if (a == 2 || a == 3) return true;
-        
-        if (dp[a] != -1) return dp[a];
-
-        for (int i=2; i*i<=a; i++) {
-            if (a % i == 0) return dp[a] = false;
-        }
-        return dp[a] = true;
-    }
     int util(int n) {
         if (n == 1) return 0;
 
         for (int i=2; i<=n; i++) {
-            if (isPrime(i) && n % i == 0) {
+            if (n % i == 0) {
                 int factor2 = n / i;
                 return i + util(n/i);
             }
@@ -24,7 +13,6 @@ public:
         return 0;
     }
     int minSteps(int n) {
-        memset(dp, -1, sizeof(dp));
 
         return util(n);
     }
