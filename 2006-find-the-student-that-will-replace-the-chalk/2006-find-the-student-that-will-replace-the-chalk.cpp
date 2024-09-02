a157773970e94d64ed8af360a437b0769d9c6f7e
M VARUN  REDDY <94187286+MVARUNREDDY8203@users.noreply.github.com>
2024-09-02 23:03:53 +0530

    Time: 50 ms (97.4%), Space: 77.1 MB (8.71%) - LeetHub

--- 2006-find-the-student-that-will-replace-the-chalk/2006-find-the-student-that-will-replace-the-chalk.cpp
+++ 2006-find-the-student-that-will-replace-the-chalk/2006-find-the-student-that-will-replace-the-chalk.cpp
@@ -1,10 +1,10 @@
 class Solution {
 public:
     int chalkReplacer(vector<int>& chalk, int k) {
+        ios_base::sync_with_stdio(false);cin.tie(nullptr);cout.tie(nullptr);
         long long int sum = 0, n = chalk.size();
         for (int i=0; i<n; i++) {
             sum += chalk[i];
-            // sum %= k;
         }
         k = k % sum;
         for (int i=0; i<n; i++) {
