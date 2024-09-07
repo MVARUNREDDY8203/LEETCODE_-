9e8394563335a634a2fd49593840f794f94d8fe6
M VARUN  REDDY <94187286+MVARUNREDDY8203@users.noreply.github.com>
2024-09-07 19:58:08 +0530

    Time: 16 ms (11.29%), Space: 12.5 MB (8%) - LeetHub

---
+++ 0402-remove-k-digits/0402-remove-k-digits.cpp
@@ -0,0 +1,33 @@
+class Solution {
+public:
+    string removeKdigits(string num, int k) {
+        if (k == num.size()) return "0";
+
+        stack<int> mono_stk;
+        int m = 0, idx = 0;
+        while (idx < num.size()) {
+            while (m < k && !mono_stk.empty() && mono_stk.top() > num[idx]) {
+                mono_stk.pop();
+                m++;
+            }
+            mono_stk.push(num[idx++]);
+        }
+        // removing unnecessary part 
+        while (mono_stk.size() > num.size() - k) {
+            mono_stk.pop();
+        }
+
+        string ans = "";
+        while (!mono_stk.empty()) {
+            ans += mono_stk.top();
+            mono_stk.pop();
+        }
+        reverse(ans.begin(), ans.end());
+        // removing leading zeros
+        string refined_ans = "";
+        idx = 0;
+        while (ans[idx] == '0') idx++;
+        ans = ans.substr(idx, num.size()-k);
+        return ans.size() == 0 ? "0" : ans;
+    }
+};
