655661d5f3858398c7d6bb20de655b62bca2429e
M VARUN  REDDY <94187286+MVARUNREDDY8203@users.noreply.github.com>
2024-09-12 23:41:09 +0530

    Time: 1996 ms (5.21%), Space: 26.7 MB (77.8%) - LeetHub

--- 0503-next-greater-element-ii/0503-next-greater-element-ii.cpp
+++ 0503-next-greater-element-ii/0503-next-greater-element-ii.cpp
@@ -1,16 +1,23 @@
 class Solution {
 public:
+#define mini -1000000001
     vector<int> nextGreaterElements(vector<int>& nums) {
         int n = nums.size();
-        vector<int> ans(n, -1);
-        for (int i=0; i<n; i++) {
-            for (int j=(i+1)%n; j<i+n; j++) {
-                if (nums[j % n] > nums[i]) {
-                    ans[i] = nums[j % n];
-                    break;
-                }
+        stack<int> mono;
+        vector<int> nge(n, mini);
+        for (int i=n*n-1; i >= 0; i--) {
+            if (nge[(i%n)] != mini) continue;
+
+            if (!mono.empty()) {
+                while (!mono.empty() && mono.top() <= nums[(i%n)]) mono.pop();
+                if (!mono.empty()) nge[(i%n)] = mono.top();
             }
+            
+            mono.push(nums[(i%n)]);
         }
-        return ans;
+        for (int i=0; i<n; i++) {
+            if (nge[i] == mini) nge[i] = -1;
+        }
+        return nge;
     }
 };
