37ad0f02103ed9c37d1fdcec6954288a0785d9dd
M VARUN  REDDY <94187286+MVARUNREDDY8203@users.noreply.github.com>
2024-09-21 15:02:59 +0530

    Time: 4 ms (88.45%) | Memory: 14 MB (51.90%) - LeetSync

--- 386-lexicographical-numbers/lexicographical-numbers.cpp
+++ 386-lexicographical-numbers/lexicographical-numbers.cpp
@@ -2,6 +2,7 @@ class Solution {
 public:
     
     vector<int> lexicalOrder(int n) {
+        ios_base::sync_with_stdio(false);cin.tie(0);cout.tie(0);
         vector<int> ans;
         function<void(int)> dfs = [&](int a) -> void {
             if (a > n) return;
