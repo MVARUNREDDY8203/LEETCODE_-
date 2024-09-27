0d50a41c927c322b7176f4c291e8c06d86e49e2a
M VARUN  REDDY <94187286+MVARUNREDDY8203@users.noreply.github.com>
2024-09-27 23:55:15 +0530

    Time: 0 ms (100%), Space: 7.5 MB (86.9%) - LeetHub

--- 0678-valid-parenthesis-string/0678-valid-parenthesis-string.cpp
+++ 0678-valid-parenthesis-string/0678-valid-parenthesis-string.cpp
@@ -1,62 +1,39 @@
 class Solution {
 public:
     bool checkValidString(string s) {
-        stack<int> ob;
-        stack<int> star;
-        int n = s.size();
-        for (int i=0; i<n; i++) {
-            if (s[i] == '(') ob.push(i);
-            else if(s[i] == ')') {
-                if (!ob.empty()) ob.pop();
-                else if (!star.empty()) star.pop();
-                else return false;
+        // function<bool(string)> is_balanced = [&](string s) {
+        //     int lpar = 0;
+        //     for (int i=0; i<s.size(); i++) {
+        //         if (s[i] == '(')
+        //     }
+        // };
+        // function<bool(int)> bt = [&](int i) {
+        //     if (i >= s.size()) {
+
+        //     }
+        // };
+
+        int lpar = 0, star = 0;
+        for (int i=0; i<s.size(); i++) {
+            if (s[i] == '(') lpar++;
+            else if (s[i] == '*') star++;
+            else {
+                if (lpar == 0 && star == 0) return false;
+                else if (lpar != 0) lpar--;
+                else star--;
             }
-            else star.push(i);
         }
-        if (ob.empty()) return true;
-        while (!ob.empty()) {
-            if (star.empty() || star.top() < ob.top()) return false;
-            star.pop();
-            ob.pop();
+        int rpar = 0;
+        star = 0;
+        for (int i=s.size()-1; i>=0; i--) {
+            if (s[i] == ')') rpar++;
+            else if (s[i] == '*') star++;
+            else {
+                if (rpar == 0 && star == 0) return false;
+                else if (rpar != 0) rpar--;
+                else star--;
+            }
         }
-        return ob.empty();
+        return true;
     }
 };
-
-
-// class Solution {
-// public:
-//     bool cvs(string s, int i, int l, vector<vector<int>> &dp) {
-//         if (i > s.size()-1) return l == 0;  
-
-//         if (dp[i][l] != 2) return dp[i][l];
-
-//         int ans = 0;
-//         if (s[i] == '(') ans = cvs(s, i+1, l+1, dp);
-//         else if (s[i] == ')') ans = (l > 0 ? cvs(s, i+1, l-1, dp) : false);
-//         else ans = cvs(s, i+1, l, dp) || cvs(s, i+1, l+1, dp) || (l > 0 ? cvs(s, i+1, l-1, dp): false);
-
-//         return dp[i][l] = ans;
-//     }
-//     bool checkValidString(string s) {
-//         vector<vector<int>> dp(103, vector<int>(103, 2));
-//         return cvs(s, 0, 0, dp);
-//     }
-// };
-
-
-
-// class Solution {
-// public:
-//     bool cvs(string s, int i, int l) {
-//         if (i > s.size()-1) return l == 0;
-
-//         if (s[i] == '(') return cvs(s, i+1, l+1);
-//         if (s[i] == ')') return l > 0 ? cvs(s, i+1, l-1) : false;
-
-//         return cvs(s, i+1, l) || cvs(s, i+1, l+1) || cvs(s, i+1, l-1);
-//     }
-//     bool checkValidString(string s) {
-//         return cvs(s, 0, 0);
-//     }
-// };
