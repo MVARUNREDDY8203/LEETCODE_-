840cf0e2684a65252357fecab34535ccb18b8721
M VARUN  REDDY <94187286+MVARUNREDDY8203@users.noreply.github.com>
2025-01-01 12:24:54 +0530

    Time: 23 ms (5.34%) | Memory: 9.6 MB (8.02%) - LeetSync

--- 1537-maximum-score-after-splitting-a-string/maximum-score-after-splitting-a-string.cpp
+++ 1537-maximum-score-after-splitting-a-string/maximum-score-after-splitting-a-string.cpp
@@ -1,13 +1,24 @@
 class Solution {
 public:
     int maxScore(string s) {
-        int ans = 0;
-        for (int i=1; i<=s.size()-1; i++) {
-            int temp_ans = 0;
-            for (int j=0; j<i; j++) if (s[j] == '0') temp_ans++;
-            for (int j=i; j<s.size(); j++) if (s[j] == '1') temp_ans++;
-            ans = max(ans, temp_ans);
+        int n = s.size();
+        vector<int> pfx_zeros(n, 0);
+        vector<int> pfx_ones(n, 0);
+        pfx_zeros[0] = s[0] == '0' ? 1 : 0;
+        for (int i=1; i<n; i++) {
+            // int add = 
+            pfx_zeros[i] = pfx_zeros[i-1] + (s[i] == '0' ? 1 : 0); 
+            cout<<pfx_zeros[i]<<" ";
         }
+
+        pfx_ones[n-1] = s[n-1] == '1' ? 1 : 0;
+        for (int i=n-2; i>=0; i--) {
+            pfx_ones[i] = pfx_ones[i+1] + (s[i] == '1' ? 1 : 0); 
+        }
+
+        int ans = 0;
+        for (int i=0; i<=n-2; i++) ans = max(ans, pfx_zeros[i] + pfx_ones[i+1]);
+
         return ans;
     }
 };
