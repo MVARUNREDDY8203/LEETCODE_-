943b7b35dbe09d5030c23a950ae3d652bc03f259
M VARUN  REDDY <94187286+MVARUNREDDY8203@users.noreply.github.com>
2025-01-03 13:20:07 +0530

    Time: 217 ms (5.06%), Space: 69.8 MB (65.45%) - LeetHub

---
+++ 2691-count-vowel-strings-in-ranges/2691-count-vowel-strings-in-ranges.cpp
@@ -0,0 +1,29 @@
+class Solution {
+public:
+    vector<int> vowelStrings(vector<string>& words, vector<vector<int>>& queries) {
+        int cache[100001];
+        memset(cache, 0, sizeof(cache));
+
+        string vowels = "aeiou";
+        int n = words.size();
+        for (int i=0; i<n; i++) {
+            int cnt = 0;
+            for (int j=0; j<5; j++) {
+                if (words[i][0] == vowels[j]) cnt++;
+                if (words[i][words[i].size()-1] == vowels[j]) cnt++;
+            }
+            if (cnt == 2) cache[i]++;
+            if (i > 0) cache[i] += cache[i-1];
+            cout<<cache[i]<<" ";
+        }
+
+        vector<int> ans;
+        for (int i=0; i<queries.size(); i++) {
+            int l = queries[i][0];
+            int r = queries[i][1];
+
+            ans.push_back(cache[r] - (l-1 >= 0 ? cache[l-1] : 0));
+        }
+        return ans;
+    }
+};
