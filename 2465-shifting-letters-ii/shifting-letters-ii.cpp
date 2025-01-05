260cfe64ffc5babf424d5459018308adab703d97
M VARUN  REDDY <94187286+MVARUNREDDY8203@users.noreply.github.com>
2025-01-05 10:40:13 +0530

    Time: 73 ms (16.18%) | Memory: 115.7 MB (5.26%) - LeetSync

---
+++ 2465-shifting-letters-ii/shifting-letters-ii.cpp
@@ -0,0 +1,17 @@
+class Solution {
+public:
+    string shiftingLetters(string s, vector<vector<int>>& shifts) {
+        unordered_map<int, int> shift;
+        for (int i=0; i<shifts.size(); i++) {
+            shift[shifts[i][0]] += (shifts[i][2] == 1 ? 1 : -1);
+            shift[shifts[i][1] + 1] += (shifts[i][2] == 1 ? -1 : 1);
+        }
+        int net_shift = 0;
+        for (int i=0; i<s.size(); i++) {
+            net_shift += shift[i];
+            s[i] = ((((s[i] - 'a') + net_shift + 26 * 100000) % 26) + 'a');
+        }
+        
+        return s;
+    }
+};
