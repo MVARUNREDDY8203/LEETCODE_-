a929bbeec801da4804ee0552b71a9d5d53310dfd
M VARUN  REDDY <94187286+MVARUNREDDY8203@users.noreply.github.com>
2025-01-06 23:45:29 +0530

    Time: 11 ms (32.26%) | Memory: 26.2 MB (72.13%) - LeetSync

--- 689-maximum-sum-of-3-non-overlapping-subarrays/maximum-sum-of-3-non-overlapping-subarrays.cpp
+++ 689-maximum-sum-of-3-non-overlapping-subarrays/maximum-sum-of-3-non-overlapping-subarrays.cpp
@@ -1,48 +1,40 @@
 class Solution {
 public:
     vector<int> maxSumOfThreeSubarrays(vector<int>& nums, int k) {
-        vector<int> prefix;
-        int window = 0;
-        for (int i=0; i<k; i++) window += nums[i];
-        prefix.push_back(window);
-        for (int i=k; i<nums.size(); i++) {
-            window += nums[i];
-            window -= nums[i-k];
-            prefix.push_back(window);
+        const int n=nums.size();
+        vector<int> ksum(n-k+1, 0), L(n-3*k+1, -1);
+        int maxKsum=ksum[0]=accumulate(nums.begin(), nums.begin()+k, 0);
+        L[0]=0;
+        for(int i=1; i<n-k+1; i++){
+            ksum[i]=ksum[i-1]-nums[i-1]+nums[i+k-1];
+            if (i>n-3*k) continue;
+            if (maxKsum<ksum[i]){
+                maxKsum=ksum[i];
+                L[i]=i;
+            }
+            else L[i]=L[i-1];
+        //    cout<<i<<"->L->"<<L[i]<<" maxKsum="<<maxKsum<<endl;
         }
-
-        vector<vector<int>> dp(nums.size(), vector<int>(4, -1));
-        function<int(int, int)> util = [&](int i, int rem) -> int {
-            if (rem <= 0) return 0;
-            if (i >= prefix.size()) return 0;
-
-            if (dp[i][rem] != -1) return dp[i][rem];
-            // dont take
-            int dont_take = util(i+1, rem);
-            int take = prefix[i] + util(i+k, rem-1);
-
-            return dp[i][rem] = max(dont_take, take);
-        };
-        util(0, 3);
-
-        vector<int> indices;
-        function <void(int, int)> dfs = [&](int i, int rem) -> void {
-            if (rem <= 0) return;
-            if (i >= prefix.size()) return;
-
-            int take = prefix[i] + util(i+k, rem-1);
-            int dont_take = util(i+1, rem);
-
-            if (take >= dont_take) {
-                indices.push_back(i);
-                dfs(i+k, rem-1);
+        vector<int> R(n-3*k+1, -1);
+        maxKsum=ksum.back();
+        R.back()=n-k;
+        for(int i=n-k-1; i>=2*k; i--){
+            if (maxKsum<=ksum[i]){// multiple answers, return the lexico smallest one
+                maxKsum=ksum[i];
+                R[i-2*k]=i;
             }
-            else {
-                dfs(i+1, rem);
-            }
-        };
-        dfs(0, 3);
-        
-        return indices;
+            else R[i-2*k]=R[i+1-2*k];
+        //    cout<<i-2*k<<"->R->"<<R[i-2*k]<<" maxKsum="<<maxKsum<<endl;
+        }
+        vector<int> ans(3,-1);
+        int max3K=0;
+        for(int i=0; i<=n-3*k; i++){
+             int sum3K=ksum[L[i]]+ksum[i+k]+ksum[R[i]];
+             if (sum3K> max3K){
+                max3K=sum3K;
+                ans={L[i], i+k, R[i]};
+             }
+        }
+        return ans;  
     }
 };
