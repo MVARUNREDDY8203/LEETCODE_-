2bc74caa2c4c9b8a60e17b35ef6a6d3282e6c0a4
M VARUN  REDDY <94187286+MVARUNREDDY8203@users.noreply.github.com>
2025-01-11 11:34:56 +0530

    Time: 1 ms (92.23%) | Memory: 11.8 MB (62.75%) - LeetSync

--- 494-target-sum/target-sum.cpp
+++ 494-target-sum/target-sum.cpp
@@ -1,20 +1,20 @@
 class Solution {
 public:
+    int dp[20][2000];
     int tar = 0;
-    map<pair<int, int>, int> dp;
-    int util (int i, int rem, vector<int>&nums) {
+    int util(int i, int rem, vector<int>&nums) {
         if (i >= nums.size()) {
             if (rem == tar) return 1;
             return 0;
         }
-
-        if (dp.count({i, rem})) return dp[{i,rem}];
         
-        return dp[{i , rem}] = util(i+1, rem + nums[i], nums) + util(i+1, rem - nums[i], nums);
+        if (dp[i][rem + 1000] != -1) return dp[i][rem + 1000];
+        
+        return dp[i][rem + 1000] = util(i+1, rem + nums[i], nums) + util(i+1, rem - nums[i], nums);
     }
     int findTargetSumWays(vector<int>& nums, int target) {
-
         tar = target;
+        memset(dp, -1, sizeof(dp));
         return util(0, 0, nums);
     }
 };
