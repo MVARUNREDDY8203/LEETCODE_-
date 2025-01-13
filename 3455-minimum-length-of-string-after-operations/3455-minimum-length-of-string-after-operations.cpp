6b3ed0cf3d25c303928b39673fe795b4e9ba5817
M VARUN  REDDY <94187286+MVARUNREDDY8203@users.noreply.github.com>
2025-01-13 09:50:39 +0530

    Time: 39 ms (54.44%), Space: 32 MB (74.19%) - LeetHub

---
+++ 3455-minimum-length-of-string-after-operations/3455-minimum-length-of-string-after-operations.cpp
@@ -0,0 +1,18 @@
+class Solution {
+public:
+    int minimumLength(string s) {
+        ios_base::sync_with_stdio(false);cin.tie(nullptr);cout.tie(nullptr);
+        
+        int freq[26];
+        memset(freq, 0, sizeof(freq));
+        int n = s.size(), ans = s.size();
+        for (int i=0; i<n; i++) {
+            freq[s[i] - 'a']++;
+            if (freq[s[i] - 'a'] == 3) {
+                ans -= 2;
+                freq[s[i] - 'a'] -= 2;
+            }
+        }
+        return ans;
+    }
+};
