ed1e35c1829d094dbd1c3e5b763aac4aba62cdac
M VARUN  REDDY <94187286+MVARUNREDDY8203@users.noreply.github.com>
2025-01-14 11:17:07 +0530

    Time: 1 ms (81.21%), Space: 86.4 MB (61.99%) - LeetHub

--- 2766-find-the-prefix-common-array-of-two-arrays/2766-find-the-prefix-common-array-of-two-arrays.cpp
+++ 2766-find-the-prefix-common-array-of-two-arrays/2766-find-the-prefix-common-array-of-two-arrays.cpp
@@ -3,25 +3,21 @@ public:
     vector<int> findThePrefixCommonArray(vector<int>& A, vector<int>& B) {
         int n = A.size();
         vector<int> ans(n, 0);
-        unordered_set<int> ua;
-        unordered_set<int> ub;
-        
-        if (A[0] == B[0]) ans[0] = 1;
-        ua.insert(A[0]);
-        ub.insert(B[0]);
+        int ui[52];
+        memset(ui, 0, sizeof(ui));
+
+        if (ui[A[0]]++);
+        if (ui[B[0]]++);
+        if (ui[B[0]] == 2) ans[0]++;
+
         for (int i=1; i<n; i++) {
             ans[i] = ans[i-1];
-            if (!ua.count(A[i]) && ub.count(A[i])) {
-                cout<<A[i]<<" "<<B[i]<<" | ";
-                ans[i]++;
-            }
-            ua.insert(A[i]);
-            if (!ub.count(B[i]) && ua.count(B[i])) {
-                cout<<A[i]<<" "<<B[i];
-                ans[i]++;
-            }
-            cout<<endl;
-            ub.insert(B[i]);
+
+            if (ui[A[i]]++);
+            if (ui[A[i]] == 2) ans[i]++;
+
+            if (ui[B[i]]++);
+            if (ui[B[i]] == 2) ans[i]++;
         }
 
         return ans;
