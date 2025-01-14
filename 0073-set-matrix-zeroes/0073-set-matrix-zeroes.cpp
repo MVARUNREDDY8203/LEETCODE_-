800a2bbfffe728a01a69e8e341a6b913772822ac
M VARUN  REDDY <94187286+MVARUNREDDY8203@users.noreply.github.com>
2025-01-14 22:42:51 +0530

    Time: 114 ms (3.77%), Space: 18.5 MB (79.71%) - LeetHub

--- 0073-set-matrix-zeroes/0073-set-matrix-zeroes.cpp
+++ 0073-set-matrix-zeroes/0073-set-matrix-zeroes.cpp
@@ -1,30 +1,46 @@
 class Solution {
 public:
-    void setZeroes(vector<vector<int>>& matrix) {
-        int m = matrix.size();
-        int n = matrix[0].size();
-
-        unordered_set<int> rows;       
-        unordered_set<int> cols; 
-        for(int i=0; i<m; i++){
-            for(int j=0; j<n; j++){
-                if(matrix[i][j] == 0){
-                    rows.insert(i);
-                    cols.insert(j);
+    void setZeroes(vector<vector<int>>& nums) {
+        int m = nums.size(), n = nums[0].size();
+        int isroot = nums[0][0];
+        int colz = 0, rowz = 0;
+        for (int i=0; i<m; i++) if (nums[i][0] == 0) {
+            colz++;
+            break;
+        }
+        for (int j=0; j<n; j++) if (nums[0][j] == 0) {
+            rowz++;
+            break;
+        } 
+        for (int i=0; i<m; i++) {
+            for (int j=0; j<n; j++) {
+                if (nums[i][j] == 0) {
+                    nums[i][0] = 0;
+                    nums[0][j] = 0;
                 }
             }
         }
-
-        for(auto i: rows){
-            for(int j=0; j<n; j++){
-                matrix[i][j] = 0;
+        for (int i=0; i<m; i++) {
+            for (int j=0; j<n; j++) {
+                cout<<nums[i][j]<<" ";
+            }
+            cout<<endl;
+        }
+        for (int j=n-1; j>0; j--) {
+            if (nums[0][j] == 0)  {
+                for (int i=0; i<m; i++) {
+                    nums[i][j] = 0;
+                }
             }
         }
-
-        for(auto j: cols){
-            for(int i=0; i<m; i++){
-                matrix[i][j] = 0;
+        for (int i=m-1; i>0; i--) {
+            if (nums[i][0] == 0) {
+                for (int j=0; j<n; j++) {
+                    nums[i][j] = 0;
+                }
             }
         }
+        if (colz) for (int i=0; i<m; i++) nums[i][0] = 0;
+        if (rowz) for (int j=0; j<n; j++) nums[0][j] = 0;
     }
 };
