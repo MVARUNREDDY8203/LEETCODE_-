b019fdb0e90173068f2509dbc2c0a705bb76444c
M VARUN  REDDY <94187286+MVARUNREDDY8203@users.noreply.github.com>
2025-01-21 10:40:13 +0530

    Time: 2 ms (64.71%), Space: 67.5 MB (52.94%) - LeetHub

--- 2145-grid-game/2145-grid-game.cpp
+++ 2145-grid-game/2145-grid-game.cpp
@@ -1,16 +1,17 @@
 class Solution {
 public:
     long long gridGame(vector<vector<int>>& grid) {
-        int n = grid[0].size();
+        const int n = grid[0].size();
         if (n == 1) return 0;
-        vector<vector<long long int>> gridsum(2, vector<long long int>(n, 0));
+        // vector<vector<long long int>> gridsum(2, vector<long long int>(n, 0));
+        long long int gridsum[2][n];
+
         gridsum[0][0] = grid[0][0];
         gridsum[1][0] = grid[1][0];
         for (int i=1; i<n; i++) {
             gridsum[0][i] = gridsum[0][i-1] + grid[0][i];
-            cout<<gridsum[0].size()<<" ";
+            // cout<<gridsum[0].size()<<" ";
             gridsum[1][i] = gridsum[1][i-1] + grid[1][i];
-            
         }
         
         long long int ans = min(gridsum[0][n-1] - gridsum[0][0], gridsum[1][n-2]);
