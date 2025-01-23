df62d6d1f81b22af15b788f95b895cf265d9cb29
M VARUN  REDDY <94187286+MVARUNREDDY8203@users.noreply.github.com>
2025-01-23 10:46:58 +0530

    Time: 0 ms (100.00%) | Memory: 26.1 MB (89.76%) - LeetSync

--- 1396-count-servers-that-communicate/count-servers-that-communicate.cpp
+++ 1396-count-servers-that-communicate/count-servers-that-communicate.cpp
@@ -2,20 +2,33 @@ class Solution {
 public:
     int countServers(vector<vector<int>>& grid) {
         unsigned char m = grid.size(), n = grid[0].size();
-        unsigned char rows[251];
-        unsigned char cols[251];
+        bool rows[251];
+        bool cols[251];
         for (unsigned char i=0; i<m; i++) {
+            unsigned char cnt = 0;
             for (unsigned char j=0; j<n; j++) {
                 if (grid[i][j]) {
-                    rows[i]++;
-                    cols[j]++;
+                    cnt++;
                 }
+                if (cnt == 2) break;
             }
+            if (cnt == 2) rows[i] = 1;
         }
+        for (unsigned char j=0; j<n; j++) {
+            unsigned char cnt = 0;
+            for (unsigned char i=0; i<m; i++) {
+                if (grid[i][j]) {
+                    cnt++;
+                }
+                if (cnt == 2) break;
+            }
+            if (cnt == 2) cols[j] = 1;
+        }
+
         short ans = 0;
         for (unsigned char i=0; i<m; i++) {
             for (unsigned char j=0; j<n; j++) {
-                if (grid[i][j] && (rows[i] > 1 || cols[j] > 1)) {
+                if (grid[i][j] && (rows[i] || cols[j])) {
                     ans++;
                 }
             }
