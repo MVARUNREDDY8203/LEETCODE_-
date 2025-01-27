5e164768bb3f56a5d7a5648be92d3561d12dec48
M VARUN  REDDY <94187286+MVARUNREDDY8203@users.noreply.github.com>
2025-01-27 10:48:22 +0530

    Time: 183 ms (19.27%) | Memory: 67.2 MB (67.79%) - LeetSync

--- 1558-course-schedule-iv/course-schedule-iv.cpp
+++ 1558-course-schedule-iv/course-schedule-iv.cpp
@@ -1,33 +1,50 @@
 class Solution {
 public:
-    vector<vector<int>> adj;
-    map<pair<int, int>, bool> dp;
-    
-    bool dfs(int src, int target) {
-        if (dp.count({src, target})) return dp[{src, target}];
-        if (src == target) return dp[{src, target}] = true;
-        if (adj[src].size() == 0) return dp[{src, target}] = false;
+    vector<bool> checkIfPrerequisite(int n, vector<vector<int>>& pre, vector<vector<int>>& queries) {
+        short cache[101][101];
+        memset(cache, -1, sizeof(cache));
 
-        bool flag = false;
-        for (auto e: adj[src]) {
-            flag |= dfs(e, target);
-            if (flag) {
-                break;
+        vector<vector<int>> graph(n+1, vector<int>(0));
+        for (int i=0; i<pre.size(); i++) {
+            graph[pre[i][0]].push_back(pre[i][1]);
+        }
+
+        bool vis[101];
+        memset(vis, 0, sizeof(vis));
+        function<bool(int, int)> dfs = [&](int i, int target) -> bool {
+            if (i == target) return true;
+            if (vis[i]) return false;
+            if (cache[i][target] != -1) return cache[i][target];
+
+            vis[i] = 1;
+            for (int j=0; j<graph[i].size(); j++) {
+                if (dfs(graph[i][j], target)) {
+                    vis[i] = 0;
+                    return cache[i][target] = true;
+                }
             }
+            vis[i] = 0;
+
+            return cache[i][target] = false;
+        };
+        
+        for (int i=0; i<3; i++) {
+            for (int j=0; j<3; j++) {
+                cout<<cache[i][j]<<" ";
+            }
+            cout<<endl;
         }
-        return dp[{src, target}] = flag;
-    }
-    vector<bool> checkIfPrerequisite(int n, vector<vector<int>>& prq, vector<vector<int>>& queries) {
-        adj.resize(n);
-        for (int i=0; i<prq.size(); i++) {
-            adj[prq[i][1]].push_back(prq[i][0]);
+
+        for (int i=0; i<n; i++) {
+            cout<<i<<" : ";
+            for (int j=0; j<graph[i].size(); j++) {
+                cout<<graph[i][j]<<" ";
+            }
+            cout<<endl;
         }
-        for (auto i: adj) {
-            cout<<i.size()<<endl;
-        }
-        vector<bool> ans;
-        for (auto e: queries) {
-            ans.push_back(dfs(e[1], e[0]));
+        vector<bool> ans(queries.size(), 0);
+        for (int i=0; i<queries.size(); i++) {
+            ans[i] = dfs(queries[i][0], queries[i][1]);
         }
         return ans;
     }
