473b3cd6161ec13f8f9994b3f681f70ec35b1af2
M VARUN  REDDY <94187286+MVARUNREDDY8203@users.noreply.github.com>
2025-01-29 10:23:44 +0530

    Time: 0 ms (100%), Space: 14.8 MB (13.24%) - LeetHub

--- 0684-redundant-connection/0684-redundant-connection.cpp
+++ 0684-redundant-connection/0684-redundant-connection.cpp
@@ -1,27 +1,39 @@
 class Solution {
 public:
-    vector<int> parents;
-    int find_parents(int u) {
-        if (parents[u] == u) return u;
-        return parents[u] = find_parents(parents[u]);
-    }
-    void union_set(int u, int v) {
-        int u_root = find_parents(u);
-        int v_root = find_parents(v);
-
-        parents[v_root] = u_root;
-    }
     vector<int> findRedundantConnection(vector<vector<int>>& edges) {
-        int n = edges.size();
-        
-        for (int i=0; i<=n; i++) parents.push_back(i);
-        
-        for (int i=0; i<n; i++) {
-            if (find_parents(edges[i][0]) == find_parents(edges[i][1])) {
-                return edges[i];
+        short n = edges.size();
+
+        vector<vector<short>> graph(n+1, vector<short>(0));
+        for (short i=0; i<n; i++) {
+            graph[edges[i][0]].push_back(edges[i][1]);
+            graph[edges[i][1]].push_back(edges[i][0]);
+        }
+
+        bool vis[1001];
+        memset(vis, false, sizeof(vis));
+
+        int a = -1, b = -1;
+        function<bool(short, short)> dfs_finder = [&](short i, short tar) -> bool {
+            if (vis[i]) return false;
+            if (i == tar) return true;
+
+            vis[i] = true;
+            for (short j=0; j<graph[i].size(); j++) {
+                if (i == a && graph[i][j] == b) continue;
+
+                if (dfs_finder(graph[i][j], tar)) return true;
             }
-            union_set(edges[i][0], edges[i][1]);
-        }        
+            vis[i] = false;
+
+            return false;
+        };
+
+        for (int j=n-1; j>=0; j--) {
+            a = edges[j][0];
+            b = edges[j][1];
+            if (dfs_finder(a, b)) return {a, b};
+        }
+
         return {};
     }
 };
