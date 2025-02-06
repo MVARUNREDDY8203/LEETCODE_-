0aa5b49fc1c0e83e11324254e15b1fa7e4156913
M VARUN  REDDY <94187286+MVARUNREDDY8203@users.noreply.github.com>
2025-02-06 11:41:39 +0530

    Time: 334 ms (36.68%) | Memory: 105.6 MB (6.27%) - LeetSync

--- 1364-tuple-with-same-product/tuple-with-same-product.cpp
+++ 1364-tuple-with-same-product/tuple-with-same-product.cpp
@@ -1,22 +1,23 @@
 class Solution {
 public:
-    unordered_map<int, long long int> fact_cache;
-    long long int fact(int i) {
-        if (i < 2) return 1;
-        if (fact_cache.count(i)) return fact_cache[i];
+    map<pair<int, int>, long long int> fact_cache;
+    long long int fact(int n, int i) {
+        if (n < 2) return 1;
+        if (fact_cache.count({n, i})) return fact_cache[{n, i}];
 
         long long int ans = 1;
-        for (int j=i; j>=2; j--) {
+        for (int j=n; j>=i; j--) {
             ans *= j;
         }
-        
-        return fact_cache[i] = ans;
+
+        return fact_cache[{n, i}] = ans;
     }
+
     map<pair<int, int>, long long int> comb_cache;
     int combination(int n, int c) {
         if (comb_cache.count({n, c})) return comb_cache[{n, c}];
 
-        return comb_cache[{n, c}] = fact(n) / (fact(n-c) * fact(c));
+        return comb_cache[{n, c}] = fact(n, c+1) / fact(n-c, 1);
     }
     int tupleSameProduct(vector<int>& nums) {
         ios_base::sync_with_stdio(false);cin.tie(nullptr);cout.tie(nullptr);
