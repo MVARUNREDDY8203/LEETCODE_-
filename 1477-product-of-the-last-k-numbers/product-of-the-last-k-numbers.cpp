9caa07c57134ff0c6cd380a8750192c010a1dda7
M VARUN  REDDY <94187286+MVARUNREDDY8203@users.noreply.github.com>
2025-02-14 12:13:26 +0530

    Time: 25 ms (47.36%) | Memory: 79.9 MB (5.54%) - LeetSync

--- 1477-product-of-the-last-k-numbers/product-of-the-last-k-numbers.cpp
+++ 1477-product-of-the-last-k-numbers/product-of-the-last-k-numbers.cpp
@@ -1,23 +1,31 @@
 class ProductOfNumbers {
 public:
-    vector<int> stream;
+    int stream[40002];
+    int prod_stream[40002];
+    int zeros = -1;
+    int idx = 1;
     ProductOfNumbers() {
-        
+        prod_stream[0] = 1;
+        stream[0] = -1;
     }
     
     void add(int num) {
-        stream.push_back(num);
+        stream[idx] = num;
+        if (num == 0) {
+            zeros = (idx);
+            prod_stream[idx] = 1;
+        } else {
+            prod_stream[idx] = prod_stream[idx-1] * stream[idx];
+        }
+        idx++;
     }
     
     int getProduct(int k) {
-        int n = stream.size()-1;
-        int prod = 1;
-        while (k) {
-            prod *= stream[n];
-            n--;
-            k--;
-        }
-        return prod;
+        int n = idx-1;
+        int first_idx = n - k + 1;
+        if (zeros >= first_idx) return 0;
+
+        return prod_stream[n] / prod_stream[n-k];
     }
 };
 
