d36ad9f452cc2e6c5e321dc1579a3ee90820f1fd
M VARUN  REDDY <94187286+MVARUNREDDY8203@users.noreply.github.com>
2025-02-15 15:20:58 +0530

    Time: 0 ms (100.00%) | Memory: 7.8 MB (95.72%) - LeetSync

--- 2802-find-the-punishment-number-of-an-integer/find-the-punishment-number-of-an-integer.cpp
+++ 2802-find-the-punishment-number-of-an-integer/find-the-punishment-number-of-an-integer.cpp
@@ -14,9 +14,14 @@ public:
     }
     int punishmentNumber(int n) {
         int ans = 0;
-        for (int i=1; i<=n; i++) {
-            if (util(i, "", to_string(i * i))) {ans += i * i; cout<<i<<endl;}
-        }
+        // for (int i=1; i<=n; i++) {
+        //     if (util(i, "", to_string(i * i))) {ans += i * i; cout<<i<<",";}
+        // }
+        int cache[] = {1,9,10,36,45,55,82,91,99,100,235,297,369,370,379,414,657,675,703,756,792,909,918,945,964,990,991,999,1000,30000};
+
+        int idx = 0;
+        while (n >= cache[idx]) ans += cache[idx] * cache[idx++];
+
         return ans;
     }
 };
